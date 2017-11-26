66ad1c37037e890b51366607df39b4889fe7044d
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2017-11-26 17:44:54 +0100

    Storage loop i DataFile class called by production main program.

--- store/OLD/store_master.bat
+++ store/OLD/store_master.bat
@@ -1,3 +1,3 @@
 @echo off
-Z:\util\WinPython\python-3.6.1.amd64\python.exe Z:\app\python\store_master.py %*
+Z:\util\WinPython\python-3.6.1.amd64\python.exe Z:\app\python\dogger\store_test.py %*
 pause
