7a8d4618cf940d83a4eee0485f1a79f0b56df4a5
Daniel Nilsson <20142935+dannil10@users.noreply.github.com>
2017-12-02 18:59:28 +0100

    Adjust class name

--- store_test.py
+++ store_test.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import store.host
 
-numpy_sql = store.host.Numpy(channels = {21,23,20,24,22})
+numpy_sql = store.host.NumpyFile(channels = {21,23,20,24,22})
 numpy_sql.run()
-=======
-import store.host
-
-numpy_sql = store.host.Numpy(channels = {21,23,20,24,22})
-numpy_sql.run()
->>>>>>> d495e0eda7e5e7b6e15d4af67034c287da483d6a
