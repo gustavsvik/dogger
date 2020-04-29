57c5d5c8273d200b0af705f877cbc1812346f158
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-04-29 10:24:12 +0000

    Update upload_local.py

--- upload_local.py
+++ upload_local.py
@@ -1,5 +1,4 @@
 import upload.host
 
 http_upload_local = upload.host.Http(channels = {20, 21, 23, 24, 40, 97, 161, 500}, start_delay = 0, ip_list = ['192.168.1.103'], max_connect_attempts = 50)
-http_upload_local.clear_channels()
 http_upload_local.run()
