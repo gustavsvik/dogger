2d27a0fa551e8c2e7d97402b759d79764aa90f5a
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-04-29 10:23:03 +0000

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,5 +1,4 @@
 import upload.host
 
 http_upload = upload.host.Http(channels = {20, 21, 23, 24, 40, 97, 98, 99, 100, 161, 500}, start_delay = 0, ip_list = ['109.74.8.89'], max_connect_attempts = 50)
-http_upload.clear_channels()
 http_upload.run()
