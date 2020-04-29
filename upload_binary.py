0467c8906286f20532b91c8075d27907566f5980
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-04-29 10:23:27 +0000

    Update upload_binary.py

--- upload_binary.py
+++ upload_binary.py
@@ -1,5 +1,4 @@
 import upload.host
 
 http_upload_binary = upload.host.Http(channels = {140, 160, 180, 600, 10002}, start_delay = 0, ip_list = ['109.74.8.89'], max_connect_attempts = 50)
-http_upload_binary.clear_channels()
 http_upload_binary.run()
