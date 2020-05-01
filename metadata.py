d04810baf935865674ee13bec7bb7fe34c18db93
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-01 15:09:04 +0000

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -1,19 +1,49 @@
 #
 
+def strip_string(val_array, label, key, default_val = ''):
+    string_val = default_val
+    try:
+        string_val = val_array[label][key].strip('\'')
+    except (ValueError, TypeError, KeyError):
+        string_val = default_val
+    return string_val
+
+
+def to_float(val, default_val = 0.0):
+    float_val = default_val
+    try:
+        float_val = float(val)
+    except (ValueError, TypeError):
+        float_val = default_val
+    return float_val
+
+
+def to_int(val, default_val = 0):
+    int_val = default_val
+    try:
+        int_val = int(float(val))
+    except (ValueError, TypeError):
+        int_val = default_val
+    return int_val
+
+
 class Local:
     pass
  
 class Configure(Local):
 
-    import os
+    #import os
     
     #DATA_FILEPATH = '/home/heta/Z/data/files/'
     #WINDOWS_FILEPATH = 'Z:/data/files/'
-    CONFIG_FILEPATH = os.path.dirname(__file__)
-    CONFIG_FILENAME = 'conf.ini'
+    #CONFIG_FILEPATH = os.path.dirname(__file__)
+    #CONFIG_FILENAME = 'conf.ini'
 
-    def __init__(self, config_filename = None):
-        self.config_filename = config_filename
+
+    def __init__(self, filepath = None, filename = None):
+    
+        self.config_filepath = filepath
+        self.config_filename = filename
 
     #def get_data_filepath(self):
     #    self.data_filepath = Configure.DATA_FILEPATH
@@ -30,87 +60,39 @@ class Configure(Local):
         import runtime
 
         conf = configparser.ConfigParser()
-        conf.read(os.path.join(Configure.CONFIG_FILEPATH, Configure.CONFIG_FILENAME))
+        filepath = os.path.dirname(__file__) #Configure.CONFIG_FILEPATH
+        filename = 'conf.ini' #Configure.CONFIG_FILENAME
+        if self.config_filepath is not None :
+            filepath = self.config_filepath
+        if self.config_filename is not None :
+            filename = self.config_filename
+        conf.read(os.path.join(filepath, filename))
         top_label = 'dogger'
 
         env = dict()
 
-        net_store_path = ''
-        local_store_path = ''
-        windows_store_path = ''
-        net_archive_path = ''
-        local_archive_path = ''
-        windows_archive_path = ''
-        sample_rate = 0
-        samples_per_chan = 0
-        image_channel_1 = 0
-        data_channel_1 = 0
-        no_of_data_channels = 0
-        archive_interval = 0
-        image_archive_interval = 0
-        data_archive_interval = 0
-        truncate_interval = 0
-        acquired_truncate_interval = 0
-        accumulated_delete_interval = 0
-        store_database_host = ''
-        store_database_user = ''
-        store_database_passwd = ''
-        store_database_db = ''
+        net_store_path = strip_string(conf, top_label, 'net_store_path', '')
+        local_store_path = strip_string(conf, top_label, 'local_store_path', '')
+        windows_store_path = strip_string(conf, top_label, 'windows_store_path', '')
+        net_archive_path = strip_string(conf, top_label, 'net_archive_path', '')
+        local_archive_path = strip_string(conf, top_label, 'local_archive_path', '')
+        windows_archive_path = strip_string(conf, top_label, 'windows_archive_path', '')
+        store_database_host = strip_string(conf, top_label, 'store_database_host', '')
+        store_database_user = strip_string(conf, top_label, 'store_database_user', '')
+        store_database_passwd = strip_string(conf, top_label, 'store_database_passwd', '')
+        store_database_db = strip_string(conf, top_label, 'store_database_db', '')
 
-        net_store_path = conf[top_label]['net_store_path'].strip('\'')
-        local_store_path = conf[top_label]['local_store_path'].strip('\'')
-        windows_store_path = conf[top_label]['windows_store_path'].strip('\'')
-        net_archive_path = conf[top_label]['net_archive_path'].strip('\'')
-        local_archive_path = conf[top_label]['local_archive_path'].strip('\'')
-        windows_archive_path = conf[top_label]['windows_archive_path'].strip('\'')
-        store_database_host = conf[top_label]['store_database_host'].strip('\'')
-        store_database_user = conf[top_label]['store_database_user'].strip('\'')
-        store_database_passwd = conf[top_label]['store_database_passwd'].strip('\'')
-        store_database_db = conf[top_label]['store_database_db'].strip('\'')
-        try :
-            sample_rate = float(conf[top_label]['sample_rate'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            samples_per_chan = float(conf[top_label]['samples_per_chan'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            image_channel_1 = int(conf[top_label]['image_channel_1'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            data_channel_1 = int(conf[top_label]['data_channel_1'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            no_of_data_channels = int(conf[top_label]['no_of_data_channels'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            archive_interval = int(conf[top_label]['archive_interval'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            image_archive_interval = int(conf[top_label]['image_archive_interval'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            data_archive_interval = int(conf[top_label]['data_archive_interval'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            truncate_interval = int(conf[top_label]['truncate_interval'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            acquired_truncate_interval = int(conf[top_label]['acquired_truncate_interval'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
-        try :
-            accumulated_delete_interval = int(conf[top_label]['accumulated_delete_interval'].strip('\''))
-        except (TypeError, ValueError, KeyError) as e :
-            runtime.logging.exception(e)
+        sample_rate = to_float(strip_string(conf, top_label, 'sample_rate'), 0.0)
+        samples_per_chan = to_float(strip_string(conf, top_label, 'samples_per_chan'), 0.0)
+        image_channel_1 = to_int(strip_string(conf, top_label, 'image_channel_1'), 0)
+        data_channel_1 = to_int(strip_string(conf, top_label, 'data_channel_1'), 0)
+        no_of_data_channels = to_int(strip_string(conf, top_label, 'no_of_data_channels'), 0)
+        archive_interval = to_int(strip_string(conf, top_label, 'archive_interval'), 0)
+        image_archive_interval = to_int(strip_string(conf, top_label, 'image_archive_interval'), 0)
+        data_archive_interval = to_int(strip_string(conf, top_label, 'data_archive_interval'), 0)
+        truncate_interval = to_int(strip_string(conf, top_label, 'truncate_interval'), 0)
+        acquired_truncate_interval = to_int(strip_string(conf, top_label, 'acquired_truncate_interval'), 0)
+        accumulated_delete_interval = to_int(strip_string(conf, top_label, 'accumulated_delete_interval'), 0)
 
         if sample_rate < 1 : sample_rate = 1
         if samples_per_chan < 1 : samples_per_chan = 1
