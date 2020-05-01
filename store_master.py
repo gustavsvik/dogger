096ed681c8fd16f9a8232728d7b744ac1a4a6e9b
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-01 15:28:08 +0000

    Update store_master.py

--- store_master.py
+++ store_master.py
@@ -1,4 +1,5 @@
 import store.host
 
-numpy_sql = store.host.NumpyFile(channels = {21,23,20,24,22,161,162,163,164})
+numpy_sql = store.host.NumpyFile(channels = {21,23,20,24,22,161,162,163,164}, config_filename = 'conf.ini')
 numpy_sql.run()
+
