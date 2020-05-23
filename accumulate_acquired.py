90e322ea24a31f2e0baefbe73bd9106d408fa35f
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-23 13:00:03 +0000

    Update accumulate_acquired.py

--- accumulate_acquired.py
+++ accumulate_acquired.py
@@ -1,17 +1,15 @@
 import time
 import pymysql
 
-#import configuration
-import runtime
-from metadata import Configure
+import gateway.runtime as rt
+import gateway.metadata as md
 
-config = Configure()
+
+config = md.Configure(filepath = '/home/heta/Z/app/python/dogger/', filename = 'conf.ini')
 env = config.get()
 
 
 time.sleep(20)
-
-#env = configuration.get()
 
 channels = [21,23,20,24]
 channels.extend(list(range(97, 113)))
@@ -52,12 +50,10 @@ while (True):
                 accumulated_bin_end_time = current_timestamp - accumulated_bin_size
 
                 sql_get_minute_data = 'SELECT ACQUIRED_TIME,ACQUIRED_VALUE FROM t_acquired_data WHERE CHANNEL_INDEX=' + str(channel_index) + ' AND ACQUIRED_TIME<' + str(accumulated_bin_end_time) + ' AND ACQUIRED_TIME>=' + str(accumulated_bin_end_time - 60) + ' ORDER BY ACQUIRED_TIME DESC'
-                #print('sql_get_minute_data', sql_get_minute_data)
+
                 with conn.cursor() as cursor :
-                    #print(sql_get_minute_data)
                     cursor.execute(sql_get_minute_data)
                     results = cursor.fetchall()
-                    #cursor.close()
                     for row in results:
                         acquired_time = row[0]
                         acquired_value = row[1]
@@ -69,29 +65,24 @@ while (True):
                 if accumulated_min_samples > 0 :
                     accumulated_min_mean = accumulated_min_value / accumulated_min_samples
 
-                #if channel_index==20 :
-                #    print('sql_get_minute_data', sql_get_minute_data, 'accumulated_min_mean', accumulated_min_mean, 'accumulated_min_samples', accumulated_min_samples)
-
                 sql_insert_accumulated_60 = 'INSERT INTO t_accumulated_data (CHANNEL_INDEX,ACCUMULATED_BIN_END_TIME,ACCUMULATED_BIN_SIZE,ACCUMULATED_NO_OF_SAMPLES,ACCUMULATED_VALUE,ACCUMULATED_TEXT,ACCUMULATED_BINARY) VALUES (%s,%s,%s,%s,%s,%s,%s)'
                 with conn.cursor() as cursor :
                     try:
                         cursor.execute(sql_insert_accumulated_60, (channel_index,accumulated_bin_end_time,accumulated_bin_size,accumulated_min_samples,accumulated_min_mean,accumulated_text,accumulated_binary))
                     except pymysql.err.IntegrityError as e:
-                        runtime.logging.exception(e)
-                    #cursor.close()
+                        rt.logging.exception(e)
+
                 accumulated_min_samples = 0
                 accumulated_min_value = 0.0
 
                 if current_minute == 1 :
 
-                    #accumulated_bin_end_time = current_timestamp
                     accumulated_bin_size = 3600
 
                     sql_get_hour_data = 'SELECT ACQUIRED_TIME,ACQUIRED_VALUE FROM t_acquired_data WHERE CHANNEL_INDEX=' + str(channel_index) + ' AND ACQUIRED_TIME<' + str(accumulated_bin_end_time) + ' AND ACQUIRED_TIME>=' + str(accumulated_bin_end_time - 3600) + ' ORDER BY ACQUIRED_TIME DESC'
                     with conn.cursor() as cursor :
                         cursor.execute(sql_get_hour_data)
                         results = cursor.fetchall()
-                        #cursor.close()
                         for row in results:
                             acquired_time = row[0]
                             acquired_value = row[1]
@@ -102,40 +93,24 @@ while (True):
                     accumulated_hour_mean = 0.0
                     if accumulated_hour_samples > 0 :
                         accumulated_hour_mean = accumulated_hour_value / accumulated_hour_samples
-
-                    #if channel_index==20 :
-                    #    print('sql_get_hour_data', sql_get_hour_data, 'accumulated_hour_mean', accumulated_hour_mean, 'accumulated_hour_samples', accumulated_hour_samples)
                         
                     sql_insert_accumulated_3600 = "INSERT INTO t_accumulated_data (CHANNEL_INDEX,ACCUMULATED_BIN_END_TIME,ACCUMULATED_BIN_SIZE,ACCUMULATED_NO_OF_SAMPLES,ACCUMULATED_VALUE,ACCUMULATED_TEXT,ACCUMULATED_BINARY) VALUES (%s,%s,%s,%s,%s,%s,%s)"
                     with conn.cursor() as cursor :
                         try:
                             cursor.execute(sql_insert_accumulated_3600, (channel_index,accumulated_bin_end_time,accumulated_bin_size,accumulated_hour_samples,accumulated_hour_mean,accumulated_text,accumulated_binary))
                         except pymysql.err.IntegrityError as e:
-                            runtime.logging.exception(e)
-                        #cursor.close()
+                            rt.logging.exception(e)
+
                     accumulated_hour_samples = 0
                     accumulated_hour_value = 0.0
 
-                    #sql_get_oldest_time = 'SELECT MIN(ACQUIRED_TIME) FROM T_ACQUIRED_DATA WHERE CHANNEL_INDEX=' + str(channel_index)
-                    #oldest_time = 0
-                    #with conn.cursor() as cursor :
-                    #    cursor.execute(sql_get_oldest_time)
-                    #    results = cursor.fetchall()
-                    #    for row in results:
-                    #        oldest_time = row[0]
-                    #runtime.logging.debug(str(oldest_time) + ' ' + str(current_timestamp) + ' ' + str(int(env['TRUNCATE_INTERVAL'] * 24 * 3600)))
-                    #if int(oldest_time) < current_timestamp - int(env['TRUNCATE_INTERVAL'] * 24 * 3600) :
-                    #    sql_truncate =  'TRUNCATE TABLE T_ACQUIRED_DATA'
-                    #    with conn.cursor() as cursor :
-                    #        cursor.execute(sql_truncate)
-                    #
-                    #sql_analyze_table = 'ANALYZE TABLE T_ACQUIRED_DATA'
-                    #with conn.cursor() as cursor :
-                    #    cursor.execute(sql_analyze_table)
-
             finally:
+            
+                try: 
+                    conn.close()
+                except NameError:
+                    pass
 
-                conn.close()
-
+                
         previous_minute = current_minute
         previous_timestamp = current_timestamp
