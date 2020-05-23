02b3376193ceff9c01a7c09209b08cbbad16a946
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-23 13:22:15 +0000

    Update store_master.py

--- store_master.py
+++ store_master.py
@@ -1,5 +1,4 @@
-import store.host
+import gateway.store
 
-
-numpy_sql = store.host.NumpyFile(channels = {21,23,20,24,22,161,162,163,164}, config_filename = 'conf.ini')
+numpy_sql = gateway.store.NumpyFile(channels = {21,23,20,24,22,161,162,163,164}, config_filepath = '/home/heta/Z/app/python/dogger/', config_filename = 'conf.ini')
 numpy_sql.run()
