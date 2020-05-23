c53bd8a596aeef44b92664ef3bc8e185ed1d78fb
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-23 13:26:59 +0000

    Update upload_binary.py

--- upload_binary.py
+++ upload_binary.py
@@ -1,4 +1,4 @@
-import upload.host
+import gateway.uplink
 
-http_upload_binary = upload.host.Http(channels = {140, 160, 180, 600, 10002}, start_delay = 0, ip_list = ['109.74.8.89'], max_connect_attempts = 50)
+http_upload_binary = gateway.uplink.HttpTask(config_filepath = '/home/heta/Z/app/python/dogger/', config_filename = 'conf.ini', channels = {140, 160, 180, 600, 10002}, start_delay = 0, ip_list = ['109.74.8.89'], max_connect_attempts = 50)
 http_upload_binary.run()
