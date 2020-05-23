c4f7d2be299bcbf20d3b3fa4d3bfd88d9f926d74
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-23 13:26:21 +0000

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,4 +1,4 @@
-import upload.host
+import gateway.uplink
 
-http_upload = upload.host.Http(channels = {20, 21, 23, 24, 40, 97, 98, 99, 100, 161, 500}, start_delay = 0, ip_list = ['109.74.8.89'], max_connect_attempts = 50)
+http_upload = gateway.uplink.HttpTask(config_filepath = '/home/heta/Z/app/python/dogger/', config_filename = 'conf.ini', channels = {20,21,23,24,40,97,98,99,100,161,500}, start_delay = 0, ip_list = ['109.74.8.89'], max_connect_attempts = 50)
 http_upload.run()
