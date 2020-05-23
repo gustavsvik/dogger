2874e7e292084fefe1072b4470378879732e8ed9
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-23 13:04:21 +0000

    Update acquire_purethermal.py

--- acquire_purethermal.py
+++ acquire_purethermal.py
@@ -1,4 +1,4 @@
-import daqc.host
+import gateway.daqc
 
-usb_cam = daqc.host.USBCam(channels = {180}, sample_rate = 1.0, start_delay = 0, video_unit = '/dev/video0', video_res = [80, 60], video_rate = 5)
+usb_cam = gateway.daqc.USBCam(config_filepath = '/home/heta/Z/app/python/dogger/', config_filename = 'conf_binary.ini', channels = {180}, sample_rate = 1.0, start_delay = 0, video_unit = '/dev/video2', video_res = [80, 60], video_rate = 5)
 usb_cam.run()
