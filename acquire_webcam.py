2ab617b209c1ac4798eae32dcd85a61478f2645a
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-23 13:06:22 +0000

    Update acquire_webcam.py

--- acquire_webcam.py
+++ acquire_webcam.py
@@ -1,4 +1,4 @@
-import daqc.host
+import gateway.daqc
 
-usb_cam = daqc.host.USBCam(channels = {160}, sample_rate = 1.0, start_delay = 10, video_unit = '/dev/video1', video_res = [1280, 720], video_rate = 5)
+usb_cam = gateway.daqc.USBCam(config_filepath = '/home/heta/Z/app/python/dogger/', config_filename = 'conf_binary.ini', channels = {160}, sample_rate = 1.0, start_delay = 10, video_unit = '/dev/video0', video_res = [1280, 720], video_rate = 5)
 usb_cam.run()
