b4fc6f2e6af8320b7f286b71f8b489ccde31205f
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-23 13:36:53 +0000

    Update uplink.py

--- gateway/uplink.py
+++ gateway/uplink.py
@@ -7,24 +7,41 @@ import pymysql
 import socket
 import http
 
-import runtime
-import metadata
+import gateway.runtime as rt
+import gateway.metadata as md
+
+
+class Task:
+
+
+    def __init__(self):
+
+        self.env = self.get_env()
+
+        
+    def get_env(self): 
+
+        config = md.Configure(filepath = self.config_filepath, filename = self.config_filename)
+        env = config.get()
+
+        return env
 
 
 
-class Http:
+class HttpTask(Task):
     
 
-    def __init__(self, channels = None, start_delay = 0, ip_list = ['0.0.0.0'], max_connect_attempts = 10):
+    def __init__(self, channels = None, start_delay = 0, ip_list = ['0.0.0.0'], max_connect_attempts = 10, config_filepath = None, config_filename = None):
 
+        self.config_filepath = config_filepath
+        self.config_filename = config_filename
         self.channels = channels
         self.start_delay = start_delay
         self.ip_list = ip_list
         self.max_connect_attempts = max_connect_attempts
 
-        self.config = metadata.Configure()
-        self.env = self.config.get()
-
+        Task.__init__(self)
+        
         self.CHANNEL_RANGE_STRING = ''
         for ch in channels: 
             self.CHANNEL_RANGE_STRING +=  str(ch) + ';;'
@@ -32,20 +49,20 @@ class Http:
         self.connect_attempts = 0
         
         self.clear_channels()
-
-          
+        
+         
     def get_requested(self, channels, ip):
 
         self.connect_attempts += 1
         if self.connect_attempts > 1:
-            runtime.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
+            rt.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
         try:
-            runtime.logging.debug("channels", channels)
+            rt.logging.debug("channels", channels)
             raw_data = requests.post('http://' + ip + '/host/get_requested.php', timeout=5, data={'channelrange': channels, 'duration': 10, 'unit': 1})
             self.connect_attempts = 0
             return raw_data
         except (requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.RequestException, requests.exceptions.ConnectionError, socket.gaierror, http.client.IncompleteRead, ConnectionResetError, requests.packages.urllib3.exceptions.ProtocolError) as e:
-            runtime.logging.debug(e)
+            rt.logging.debug(e)
             time.sleep(10)
             if self.connect_attempts < self.max_connect_attempts:
                 self.get_requested(channels, ip)
@@ -57,14 +74,14 @@ class Http:
 
         self.connect_attempts += 1
         if self.connect_attempts > 1:
-            runtime.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
+            rt.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
         try:
             raw_data = requests.post('http://' + ip + '/host/set_requested.php', timeout=5, data={'returnstring': data_string})
-            runtime.logging.debug("data_string", data_string)
+            rt.logging.debug("data_string", data_string)
             self.connect_attempts = 0
             return raw_data
         except (requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.RequestException, requests.exceptions.ConnectionError, socket.gaierror, http.client.IncompleteRead, ConnectionResetError, requests.packages.urllib3.exceptions.ProtocolError) as e:
-            runtime.logging.debug(e)
+            rt.logging.debug(e)
             time.sleep(10)
             if self.connect_attempts < self.max_connect_attempts:
                 self.set_requested(data_string, ip)
@@ -76,14 +93,14 @@ class Http:
 
         self.connect_attempts += 1
         if self.connect_attempts > 1:
-            runtime.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
+            rt.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
         try:
-            runtime.logging.debug("channels", channels)
+            rt.logging.debug("channels", channels)
             raw_data = requests.post('http://' + ip + '/host/clear_data_requests.php', timeout=5, data={'channelrange': channels})
             self.connect_attempts = 0
             return raw_data
         except (requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.RequestException, requests.exceptions.ConnectionError, socket.gaierror, http.client.IncompleteRead, ConnectionResetError, requests.packages.urllib3.exceptions.ProtocolError) as e:
-            runtime.logging.debug(e)
+            rt.logging.debug(e)
             time.sleep(10)
             if self.connect_attempts < self.max_connect_attempts:
                 self.clear_data_requests(channels, ip)
@@ -97,14 +114,14 @@ class Http:
 
             cleared_channels = ""
             r_clear = self.clear_data_requests(self.CHANNEL_RANGE_STRING, ip)
-            runtime.logging.debug("r_clear", r_clear)
+            rt.logging.debug("r_clear", r_clear)
             if r_clear is not None:
                 try:
                     requested_data = r_clear.json()
                     cleared_channels = requested_data['returnstring']
                 except ValueError as e:  # includes simplejson.decoder.JSONDecodeError
-                    runtime.logging.debug('Decoding JSON has failed', e)
-            runtime.logging.debug("cleared_channels",cleared_channels)
+                    rt.logging.debug('Decoding JSON has failed', e)
+            rt.logging.debug("cleared_channels",cleared_channels)
 
 
     def run(self):
@@ -115,14 +132,14 @@ class Http:
 
                 data_string = ""
                 r_get = self.get_requested(self.CHANNEL_RANGE_STRING, ip)
-                runtime.logging.debug("r_get", r_get)
+                rt.logging.debug("r_get", r_get)
                 if r_get is not None:
                     try:
                         requested_data = r_get.json()
                         data_string = requested_data['returnstring']
                     except ValueError as e:  # includes simplejson.decoder.JSONDecodeError
-                        runtime.logging.debug('Decoding JSON has failed', e)
-                runtime.logging.debug("data_string",data_string)
+                        rt.logging.debug('Decoding JSON has failed', e)
+                rt.logging.debug("data_string",data_string)
 
                 try:
 
@@ -143,13 +160,13 @@ class Http:
                         timestamp_list = channel_timestamps[1::2]
                         
                     for channel_index in range(len(channel_list)):
-                        print(channel_list[channel_index][0])
-                        print(timestamp_list[channel_index][:-1])
+                        #print(channel_list[channel_index][0])
+                        #print(timestamp_list[channel_index][:-1])
 
                         requested_timestamps = [int(timestamp_string) for timestamp_string in timestamp_list[channel_index][:-1]] 
-                        print(requested_timestamps)
+                        #print(requested_timestamps)
                         channel_string = channel_list[channel_index][0]
-                        print(channel_string)
+                        #print(channel_string)
 
                         if not requested_timestamps :
                             pass
@@ -161,14 +178,14 @@ class Http:
                             sql_timestamps += ")"
 
                             sql_get_values = "SELECT AD.ACQUIRED_TIME,AD.ACQUIRED_VALUE,AD.ACQUIRED_SUBSAMPLES,AD.ACQUIRED_BASE64 FROM t_acquired_data AD WHERE AD.CHANNEL_INDEX=" + channel_string + " AND AD.ACQUIRED_TIME IN " + sql_timestamps
-                            runtime.logging.debug("sql_get_values",sql_get_values)
-                            print("sql_get_values",sql_get_values)
+                            rt.logging.debug("sql_get_values",sql_get_values)
+                            #print("sql_get_values",sql_get_values)
                             return_string += channel_string + ";"
                             with conn.cursor() as cursor :
                                 try:
                                     cursor.execute(sql_get_values)
                                 except (pymysql.err.IntegrityError, pymysql.err.InternalError) as e:
-                                    runtime.logging.debug(e)
+                                    rt.logging.debug(e)
                                 results = cursor.fetchall()
                                 for row in results:
                                     acquired_time = row[0]
@@ -176,7 +193,7 @@ class Http:
                                     acquired_subsamples = row[2]
                                     acquired_base64 = row[3]
                                     base64_string = acquired_base64.decode('utf-8')
-                                    runtime.logging.debug('Channel: ', channel_string, ', Value: ', acquired_value, ', Timestamp: ', acquired_time, ', Sub-samples: ', acquired_subsamples, ', base64: ', acquired_base64)
+                                    rt.logging.debug('Channel: ', channel_string, ', Value: ', acquired_value, ', Timestamp: ', acquired_time, ', Sub-samples: ', acquired_subsamples, ', base64: ', acquired_base64)
                                     if acquired_time in requested_timestamps:
                                         requested_timestamps.remove(acquired_time)
                                     return_string += str(acquired_time) + "," + str(acquired_value) + "," + str(acquired_subsamples) + "," + str(base64_string) + ","
@@ -187,16 +204,16 @@ class Http:
                                     r_clear = self.clear_data_requests(channel_string + ';;', ip)
 
                 except (pymysql.err.OperationalError, pymysql.err.Error) as e:
-                    runtime.logging.debug(e)
+                    rt.logging.debug(e)
 
                 finally:
 
                     try:
                         conn.close()
                     except pymysql.err.Error as e:
-                        runtime.logging.debug(e)
+                        rt.logging.debug(e)
 
-                    runtime.logging.debug("return_string", return_string)
+                    rt.logging.debug("return_string", return_string)
 
                     r_post = self.set_requested(return_string, ip)
 
