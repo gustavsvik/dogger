6195c567f2a1ec838c312c19f21e02482ee0bd0a
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-05-31 21:29:45 +0000

    Update uplink.py

--- gateway/uplink.py
+++ gateway/uplink.py
@@ -9,39 +9,52 @@ import http
 
 import gateway.runtime as rt
 import gateway.metadata as md
+import gateway.task as t
 
 
-class Task:
+
+class Uplink(t.StoreUplink):
 
 
     def __init__(self):
 
         self.env = self.get_env()
-
+        if self.ip_list is None: self.ip_list = self.env['IP_LIST']
+        if self.cloud_api_url is None: self.cloud_api_url = self.env['CLOUD_API_URL']
+        if self.max_connect_attempts is None: self.max_connect_attempts = self.env['MAX_CONNECT_ATTEMPTS']
         
-    def get_env(self): 
-
-        config = md.Configure(filepath = self.config_filepath, filename = self.config_filename)
-        env = config.get()
-
-        return env
+        t.StoreUplink.__init__(self)
 
 
 
-class HttpTask(Task):
+class Http(Uplink):
     
 
-    def __init__(self, channels = None, start_delay = 0, ip_list = ['0.0.0.0'], max_connect_attempts = 10, config_filepath = None, config_filename = None):
+    def __init__(self, channels = None, start_delay = None, gateway_database_connection = None, ip_list = None, cloud_api_url = None, max_connect_attempts = None, config_filepath = None, config_filename = None):
+
+    
+        self.channels = channels
+        self.start_delay = start_delay
+        self.gateway_database_connection = gateway_database_connection
+        self.ip_list = ip_list
+        self.cloud_api_url = cloud_api_url
+        self.max_connect_attempts = max_connect_attempts
 
         self.config_filepath = config_filepath
         self.config_filename = config_filename
-        self.channels = channels
-        self.start_delay = start_delay
-        self.ip_list = ip_list
-        self.max_connect_attempts = max_connect_attempts
-
-        Task.__init__(self)
         
+        self.env = self.get_env()
+
+        Uplink.__init__(self)
+
+        #print("self.channels", self.channels)
+        #print("self.start_delay", self.start_delay)
+        #print("self.gateway_database_connection", self.gateway_database_connection)
+        #print("self.ip_list", self.ip_list)
+        #print("self.cloud_api_url", self.cloud_api_url)
+        #print("self.max_connect_attempts", self.max_connect_attempts)
+
+
         self.CHANNEL_RANGE_STRING = ''
         for ch in channels: 
             self.CHANNEL_RANGE_STRING +=  str(ch) + ';;'
@@ -50,7 +63,15 @@ class HttpTask(Task):
         
         self.clear_channels()
         
-         
+
+    #def get_env(self): 
+    #
+    #    config = md.Configure(filepath = self.config_filepath, filename = self.config_filename)
+    #    env = config.get()
+    #
+    #    return env        
+
+
     def get_requested(self, channels, ip):
 
         self.connect_attempts += 1
@@ -58,7 +79,7 @@ class HttpTask(Task):
             rt.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
         try:
             rt.logging.debug("channels", channels)
-            raw_data = requests.post('http://' + ip + '/host/get_requested.php', timeout=5, data={'channelrange': channels, 'duration': 10, 'unit': 1})
+            raw_data = requests.post("http://" + ip + self.cloud_api_url + "get_requested.php", timeout = 5, data = {'channelrange': channels, 'duration': 10, 'unit': 1})
             self.connect_attempts = 0
             return raw_data
         except (requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.RequestException, requests.exceptions.ConnectionError, socket.gaierror, http.client.IncompleteRead, ConnectionResetError, requests.packages.urllib3.exceptions.ProtocolError) as e:
@@ -76,7 +97,7 @@ class HttpTask(Task):
         if self.connect_attempts > 1:
             rt.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
         try:
-            raw_data = requests.post('http://' + ip + '/host/set_requested.php', timeout=5, data={'returnstring': data_string})
+            raw_data = requests.post("http://" + ip + self.cloud_api_url + "set_requested.php", timeout = 5, data = {'returnstring': data_string})
             rt.logging.debug("data_string", data_string)
             self.connect_attempts = 0
             return raw_data
@@ -96,7 +117,7 @@ class HttpTask(Task):
             rt.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
         try:
             rt.logging.debug("channels", channels)
-            raw_data = requests.post('http://' + ip + '/host/clear_data_requests.php', timeout=5, data={'channelrange': channels})
+            raw_data = requests.post("http://" + ip + self.cloud_api_url + "clear_data_requests.php", timeout = 5, data = {'channelrange': channels})
             self.connect_attempts = 0
             return raw_data
         except (requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.RequestException, requests.exceptions.ConnectionError, socket.gaierror, http.client.IncompleteRead, ConnectionResetError, requests.packages.urllib3.exceptions.ProtocolError) as e:
@@ -112,7 +133,7 @@ class HttpTask(Task):
 
         for ip in self.ip_list :
 
-            cleared_channels = ""
+            cleared_channels = ''
             r_clear = self.clear_data_requests(self.CHANNEL_RANGE_STRING, ip)
             rt.logging.debug("r_clear", r_clear)
             if r_clear is not None:
@@ -120,7 +141,7 @@ class HttpTask(Task):
                     requested_data = r_clear.json()
                     cleared_channels = requested_data['returnstring']
                 except ValueError as e:  # includes simplejson.decoder.JSONDecodeError
-                    rt.logging.debug('Decoding JSON has failed', e)
+                    rt.logging.debug("Decoding JSON has failed", e)
             rt.logging.debug("cleared_channels",cleared_channels)
 
 
@@ -130,7 +151,7 @@ class HttpTask(Task):
 
             for ip in self.ip_list :
 
-                data_string = ""
+                data_string = ''
                 r_get = self.get_requested(self.CHANNEL_RANGE_STRING, ip)
                 rt.logging.debug("r_get", r_get)
                 if r_get is not None:
@@ -138,12 +159,15 @@ class HttpTask(Task):
                         requested_data = r_get.json()
                         data_string = requested_data['returnstring']
                     except ValueError as e:  # includes simplejson.decoder.JSONDecodeError
-                        rt.logging.debug('Decoding JSON has failed', e)
+                        rt.logging.debug("Decoding JSON has failed", e)
                 rt.logging.debug("data_string",data_string)
 
-                try:
+                conn = None 
+                return_string = None
 
-                    conn = pymysql.connect(host=self.env['STORE_DATABASE_HOST'], user=self.env['STORE_DATABASE_USER'], passwd=self.env['STORE_DATABASE_PASSWD'], db=self.env['STORE_DATABASE_DB'], autocommit=True)
+                try:
+                    conn_data = self.gateway_database_connection
+                    conn = pymysql.connect(host = conn_data['host'], user = conn_data['user'], passwd = conn_data['passwd'], db = conn_data['db'], autocommit = True)
 
                     return_string = ""
 
@@ -155,32 +179,27 @@ class HttpTask(Task):
                     channel_list = []
                     timestamp_list = []
                     if data_string is not None:
-                        channel_timestamps = [channel_string.split(",") for channel_string in data_string.split(";")]
+                        channel_timestamps = [channel_string.split(',') for channel_string in data_string.split(';')]
                         channel_list = channel_timestamps[0::2][:-1]
                         timestamp_list = channel_timestamps[1::2]
                         
                     for channel_index in range(len(channel_list)):
-                        #print(channel_list[channel_index][0])
-                        #print(timestamp_list[channel_index][:-1])
 
                         requested_timestamps = [int(timestamp_string) for timestamp_string in timestamp_list[channel_index][:-1]] 
-                        #print(requested_timestamps)
                         channel_string = channel_list[channel_index][0]
-                        #print(channel_string)
 
                         if not requested_timestamps :
                             pass
                         else :
-                            sql_timestamps = "("
+                            sql_timestamps = '('
                             for timestamp in requested_timestamps[:-1] :
-                                sql_timestamps += str(timestamp) + ","
+                                sql_timestamps += str(timestamp) + ','
                             sql_timestamps += str(requested_timestamps[-1])
-                            sql_timestamps += ")"
+                            sql_timestamps += ')'
 
                             sql_get_values = "SELECT AD.ACQUIRED_TIME,AD.ACQUIRED_VALUE,AD.ACQUIRED_SUBSAMPLES,AD.ACQUIRED_BASE64 FROM t_acquired_data AD WHERE AD.CHANNEL_INDEX=" + channel_string + " AND AD.ACQUIRED_TIME IN " + sql_timestamps
                             rt.logging.debug("sql_get_values",sql_get_values)
-                            #print("sql_get_values",sql_get_values)
-                            return_string += channel_string + ";"
+                            return_string += channel_string + ';'
                             with conn.cursor() as cursor :
                                 try:
                                     cursor.execute(sql_get_values)
@@ -193,11 +212,11 @@ class HttpTask(Task):
                                     acquired_subsamples = row[2]
                                     acquired_base64 = row[3]
                                     base64_string = acquired_base64.decode('utf-8')
-                                    rt.logging.debug('Channel: ', channel_string, ', Value: ', acquired_value, ', Timestamp: ', acquired_time, ', Sub-samples: ', acquired_subsamples, ', base64: ', acquired_base64)
+                                    rt.logging.debug("Channel: ", channel_string, ", Value: ", acquired_value, ", Timestamp: ", acquired_time, ", Sub-samples: ", acquired_subsamples, ", base64: ", acquired_base64)
                                     if acquired_time in requested_timestamps:
                                         requested_timestamps.remove(acquired_time)
-                                    return_string += str(acquired_time) + "," + str(acquired_value) + "," + str(acquired_subsamples) + "," + str(base64_string) + ","
-                            return_string += ";"
+                                    return_string += str(acquired_time) + ',' + str(acquired_value) + ',' + str(acquired_subsamples) + ',' + str(base64_string) + ','
+                            return_string += ';'
 
                             if len(requested_timestamps) > 0:
                                 if min(requested_timestamps) < int(time.time()) - 3600:
@@ -209,7 +228,7 @@ class HttpTask(Task):
                 finally:
 
                     try:
-                        conn.close()
+                        if conn is not None: conn.close()
                     except pymysql.err.Error as e:
                         rt.logging.debug(e)
 
