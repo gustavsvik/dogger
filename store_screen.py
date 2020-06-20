20dda1996b5180ffd760ee7b2b0250ebb069089c
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-06-20 15:48:51 +0000

    Update store_screen.py

--- store_screen.py
+++ store_screen.py
@@ -4,7 +4,7 @@ import gateway.store
 image_sql = gateway.store.ScreenshotFile(
     channels = {600}, 
     start_delay = 0, 
-    gateway_database_connection = {"host": "localhost", "user": "root", "passwd": "admin", "db": "test"}, 
+    gateway_database_connection = {"host": "host", "user": "user", "passwd": "passwd", "db": "db"}, 
     file_path = '/home/heta/Z/data/files/screenshots/',
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf_screen.ini')
