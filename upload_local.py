02c40fac069e1472eee68ffb90498b861b037271
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-06-20 15:53:30 +0000

    Update upload_local.py

--- upload_local.py
+++ upload_local.py
@@ -4,10 +4,10 @@ import gateway.uplink
 http_upload_local = gateway.uplink.Replicate(
     channels = {20,21,23,24,40,97,161,500}, 
     start_delay = 0, 
-    gateway_database_connection = {"host": "localhost", "user": "root", "passwd": "admin", "db": "test"}, 
+    gateway_database_connection = {"host": "host", "user": "user", "passwd": "passwd", "db": "db"}, 
     host_api_url = '/host/', 
     client_api_url = '/client/', 
-    ip_list = ['192.168.1.103'], 
+    ip_list = ['127.0.0.1'], 
     max_connect_attempts = 50,
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf_upload.ini')
