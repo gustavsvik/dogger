a0a84f814047cd30e0f423e8772c78e8dee35f90
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 17:32:58 +0000

    Update upload_binary_local.py

--- upload_binary_local.py
+++ upload_binary_local.py
@@ -4,12 +4,10 @@ import gateway.uplink
 http_upload_binary_local = gateway.uplink.Replicate(
     channels = {140,160,180,600,10002}, 
     start_delay = 0, 
-    gateway_database_connection = {"host": "host", "user": "user", "passwd": "passwd", "db": "db"}, 
     host_api_url = '/host/', 
     client_api_url = '/client/', 
-    ip_list = ['127.0.0.1'], 
     max_connect_attempts = 50,
     config_filepath = '/home/heta/Z/app/python/dogger/', 
-    config_filename = 'conf_upload.ini')
+    config_filename = 'conf_local.ini')
 
 http_upload_binary_local.run()
