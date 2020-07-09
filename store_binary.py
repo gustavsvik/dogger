6d256234e24ea04b75218290f0f1f19b45d2208b
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 17:26:11 +0000

    Update store_binary.py

--- store_binary.py
+++ store_binary.py
@@ -4,9 +4,8 @@ import gateway.store
 image_sql = gateway.store.ImageFile(
     channels = {140,141,160,161,180,181}, 
     start_delay = 0, 
-    gateway_database_connection = {"host": "host", "user": "user", "passwd": "passwd", "db": "db"}, 
     file_path = '/home/heta/Z/data/files/images/',
     config_filepath = '/home/heta/Z/app/python/dogger/', 
-    config_filename = 'conf_binary.ini')
+    config_filename = 'conf.ini')
 
 image_sql.run()
