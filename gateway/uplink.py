607ef23573e351bec5db6221fb7ec98b52658799
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 17:40:37 +0000

    Update uplink.py

--- gateway/uplink.py
+++ gateway/uplink.py
@@ -10,6 +10,7 @@ import http
 import gateway.runtime as rt
 import gateway.metadata as md
 import gateway.task as t
+import gateway.database as db
 
 
 
@@ -30,22 +31,11 @@ class Uplink(t.StoreUplink):
 class Http(Uplink):
     
 
-    def __init__(self, channels = None, start_delay = None, gateway_database_connection = None, ip_list = None, host_api_url = None, client_api_url = None, max_connect_attempts = None, config_filepath = None, config_filename = None):
-
-        self.channels = channels
-        self.start_delay = start_delay
-        self.gateway_database_connection = gateway_database_connection
-        self.ip_list = ip_list
-        self.host_api_url = host_api_url
-        self.client_api_url = client_api_url
-        self.max_connect_attempts = max_connect_attempts
-
-        self.config_filepath = config_filepath
-        self.config_filename = config_filename
+    def __init__(self) :
 
         Uplink.__init__(self)
         
-        self.channel_range_string = ';;'.join([str(ch) for ch in channels]) + ';;'
+        self.channel_range_string = ';;'.join([str(ch) for ch in self.channels]) + ';;'
 
         self.connect_attempts = 0
 
@@ -57,9 +47,7 @@ class Http(Uplink):
             rt.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
         try:
             rt.logging.debug("self.channel_range_string", self.channel_range_string)
-            #print("self.channel_range_string", self.channel_range_string)
             raw_data = requests.post("http://" + ip + self.host_api_url + "get_requested.php", timeout = 5, data = {"channelrange": self.channel_range_string, "duration": 10, "unit": 1})
-            #print("raw_data.request.headers", raw_data.request.headers)
             self.connect_attempts = 0
             return raw_data
         except (requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.RequestException, requests.exceptions.ConnectionError, socket.gaierror, http.client.IncompleteRead, ConnectionResetError, requests.packages.urllib3.exceptions.ProtocolError) as e:
@@ -77,15 +65,12 @@ class Http(Uplink):
         if self.connect_attempts > 1:
             rt.logging.debug("Retrying connection, attempt " + str(self.connect_attempts))
         try:
-            #print("http://" + ip + self.host_api_url + "set_requested.php")
-            #print(data_string)
             raw_data = requests.post("http://" + ip + self.host_api_url + "set_requested.php", timeout = 5, data = {"returnstring": data_string})
             rt.logging.debug("data_string", data_string)
             self.connect_attempts = 0
             return raw_data
         except (requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.RequestException, requests.exceptions.ConnectionError, socket.gaierror, http.client.IncompleteRead, ConnectionResetError, requests.packages.urllib3.exceptions.ProtocolError) as e:
             rt.logging.debug(e)
-            #print(e)
             time.sleep(10)
             if self.connect_attempts < self.max_connect_attempts:
                 self.set_requested(data_string, ip)
@@ -130,8 +115,28 @@ class Http(Uplink):
             else:
                 exit(-1)
 
-            
-            
+
+
+class DirectUpload(Http):
+
+
+    def __init__(self, channels = None, start_delay = None, ip_list = None, host_api_url = '/host/', client_api_url = '/client/', max_connect_attempts = 50, config_filepath = None, config_filename = None):
+
+        self.channels = channels
+        self.start_delay = start_delay
+        self.ip_list = ip_list
+        self.host_api_url = host_api_url
+        self.client_api_url = client_api_url
+        self.max_connect_attempts = max_connect_attempts
+
+        self.config_filepath = config_filepath
+        self.config_filename = config_filename
+        
+        self.env = self.get_env()
+        Http.__init__(self)
+
+
+
 class Replicate(Http):
 
 
@@ -149,8 +154,9 @@ class Replicate(Http):
         self.config_filename = config_filename
         
         self.env = self.get_env()
-        Http.__init__(self, self.channels, self.start_delay, self.gateway_database_connection, self.ip_list, self.host_api_url, self.client_api_url, self.max_connect_attempts, self.config_filepath, self.config_filename)
-        #print("self.ip_list Replicate", self.ip_list)
+        Http.__init__(self)
+
+        self.sql = db.SQL(gateway_database_connection = self.gateway_database_connection, config_filepath = self.config_filepath, config_filename = self.config_filename)
 
         self.connect_attempts = 0
         
@@ -182,6 +188,7 @@ class Replicate(Http):
                 data_string = ''
                 r_get = self.get_requested(ip)
                 rt.logging.debug("r_get", r_get)
+
                 if r_get is not None:
                     try:
                         requested_data = r_get.json()
@@ -190,12 +197,11 @@ class Replicate(Http):
                         rt.logging.debug("Decoding JSON has failed", e)
                 rt.logging.debug("data_string",data_string)
 
-                conn = None 
                 return_string = None
 
                 try:
-                    conn_data = self.gateway_database_connection
-                    conn = pymysql.connect(host = conn_data['host'], user = conn_data['user'], passwd = conn_data['passwd'], db = conn_data['db'], autocommit = True)
+
+                    conn = self.sql.connect_db()
 
                     return_string = ""
 
@@ -210,7 +216,7 @@ class Replicate(Http):
                         channel_timestamps = [channel_string.split(',') for channel_string in data_string.split(';')]
                         channel_list = channel_timestamps[0::2][:-1]
                         timestamp_list = channel_timestamps[1::2]
-                        
+
                     for channel_index in range(len(channel_list)):
 
                         requested_timestamps = [int(ts_string) for ts_string in timestamp_list[channel_index][:-1]] 
@@ -223,6 +229,7 @@ class Replicate(Http):
 
                             sql_get_values = "SELECT AD.ACQUIRED_TIME,AD.ACQUIRED_VALUE,AD.ACQUIRED_SUBSAMPLES,AD.ACQUIRED_BASE64 FROM t_acquired_data AD WHERE AD.CHANNEL_INDEX=" + channel_string + " AND AD.ACQUIRED_TIME IN " + sql_timestamps
                             rt.logging.debug("sql_get_values",sql_get_values)
+
                             return_string += channel_string + ';'
                             with conn.cursor() as cursor :
                                 try:
@@ -251,14 +258,11 @@ class Replicate(Http):
 
                 finally:
 
-                    try:
-                        if conn is not None: conn.close()
-                    except pymysql.err.Error as e:
-                        rt.logging.debug(e)
+                    self.sql.close_db_connection()
 
                     rt.logging.debug("return_string", return_string)
-
-                    r_post = self.set_requested(return_string, ip)
+                    if return_string is not None :
+                        r_post = self.set_requested(return_string, ip)
 
 
             time.sleep(1.0)
