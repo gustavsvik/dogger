128b3291645a5429eaa31696a877a0691643f68e
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 17:27:38 +0000

    Update store_current.py

--- store_current.py
+++ store_current.py
@@ -4,9 +4,8 @@ import gateway.store
 numpy_sql = gateway.store.NumpyFile(
     channels = {97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112}, 
     start_delay = 0, 
-    gateway_database_connection = {"host": "host", "user": "user", "passwd": "passwd", "db": "db"}, 
     file_path = '/home/heta/Z/data/files/current/',
     config_filepath = '/home/heta/Z/app/python/dogger/', 
-    config_filename = 'conf_current.ini')
+    config_filename = 'conf.ini')
 
 numpy_sql.run()
