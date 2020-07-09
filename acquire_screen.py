f66597052d5fb73567925ca6140164daa6527313
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 17:16:35 +0000

    Update acquire_screen.py

--- acquire_screen.py
+++ acquire_screen.py
@@ -1,16 +1,11 @@
 import gateway.daqc
 
 
-screenshot = gateway.daqc.Screenshot(
+screenshot = gateway.daqc.ScreenshotUpload(
     channels = {600}, 
-    start_delay = 0, 
     sample_rate = 1.0, 
-    file_path = None, 
-    archive_file_path = None, 
-    video_res = None, 
-    ip_list = ['109.74.8.89'],
-    crop = [0,184,1243,1040],
-    config_filepath = None, 
-    config_filename = None)
+    crop = [0,184,1243,1040], 
+    config_filepath = 'C:\\screenshot\\',
+    config_filename = 'conf.ini')
 
 screenshot.run()
