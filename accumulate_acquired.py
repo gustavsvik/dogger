22809a81525d2e8a5a729dd1a80a78e8b96a97cd
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 14:26:04 +0000

    Update accumulate_acquired.py

--- accumulate_acquired.py
+++ accumulate_acquired.py
@@ -4,7 +4,6 @@ import gateway.store
 accumulate_acquired = gateway.store.Accumulate(
     channels = {20,21,22,23,24,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112}, 
     start_delay = 20, 
-    gateway_database_connection = {"host": "localhost", "user": "root", "passwd": "admin", "db": "test"}, 
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf.ini')
 
