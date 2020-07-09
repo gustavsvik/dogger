c96ad0ad3816368e54d046236a2cd6b4eb86a3e1
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 14:26:55 +0000

    Update acquire_current.py

--- acquire_current.py
+++ acquire_current.py
@@ -3,8 +3,6 @@ import gateway.daqc
 
 acquire_current = gateway.daqc.AcquireCurrent(
     file_path = 'Z:\\data\\files\\current\\', 
-    archive_file_path = None, 
-    config_filepath = 'Z:\\app\\python\\dogger\\', 
-    config_filename = 'conf_current.ini')
+    archive_file_path = None)
 
 acquire_current.run()
