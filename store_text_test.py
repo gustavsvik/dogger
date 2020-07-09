8281a85b2e4b27f67f4dd3fe9d78f340e82fd773
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 17:30:18 +0000

    Update store_text_test.py

--- store_text_test.py
+++ store_text_test.py
@@ -4,7 +4,6 @@ import gateway.store
 text_sql = gateway.store.TextFile(
     channels = {65533}, 
     start_delay = 0, 
-    gateway_database_connection = {"host": "host", "user": "user", "passwd": "passwd", "db": "db"}, 
     file_path = '/home/heta/Z/data/files/',
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf.ini')
