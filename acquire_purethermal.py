496deed9b0b3d06fa3d6ec82981b73f8a6b742f9
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 14:38:11 +0000

    Update acquire_purethermal.py

--- acquire_purethermal.py
+++ acquire_purethermal.py
@@ -8,8 +8,6 @@ usb_cam = gateway.daqc.USBCam(
     archive_file_path = None, 
     video_unit = '/dev/video2', 
     video_res = [1280, 720], 
-    video_rate = 5,
-    config_filepath = '/home/heta/Z/app/python/dogger/', 
-    config_filename = 'conf_binary.ini')
+    video_rate = 5)
 
 usb_cam.run()
