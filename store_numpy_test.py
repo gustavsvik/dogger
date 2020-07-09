1c563d3166b81a8dfb5a4c2dd94d74559a99acaf
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-09 17:28:58 +0000

    Update store_numpy_test.py

--- store_numpy_test.py
+++ store_numpy_test.py
@@ -4,7 +4,6 @@ import gateway.store
 numpy_sql = gateway.store.NumpyFile(
     channels = {65534}, 
     start_delay = 0, 
-    gateway_database_connection = {"host": "host", "user": "user", "passwd": "passwd", "db": "db"}, 
     file_path = '/home/heta/Z/data/files/',
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf.ini')
