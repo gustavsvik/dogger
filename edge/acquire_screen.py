4df8831082dcac1739670d231cdda15240b85026
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-11 18:27:49 +0000

    Update acquire_screen.py

--- edge/acquire_screen.py
+++ edge/acquire_screen.py
@@ -3,8 +3,10 @@ import gateway.daqc
 
 screenshot = gateway.daqc.ScreenshotUpload(
     channels = {600}, 
+    start_delay = 10, 
     sample_rate = 1.0, 
     crop = [0,184,1243,1040], 
+    video_quality = 80, 
     config_filepath = 'C:\\screenshot\\',
     config_filename = 'conf.ini')
 
