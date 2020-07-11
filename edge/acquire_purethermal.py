df0cd15395a553443c638cc5f69407193bde1bb8
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-11 16:39:54 +0000

    Update acquire_purethermal.py

--- edge/acquire_purethermal.py
+++ edge/acquire_purethermal.py
@@ -1,13 +1,15 @@
 import gateway.daqc
 
+
 usb_cam = gateway.daqc.USBCam(
     channels = {180}, 
     start_delay = 10, 
     sample_rate = 1.0, 
     file_path = '/home/heta/Z/data/files/images/', 
-    archive_file_path = None, 
     video_unit = '/dev/video2', 
     video_res = [1280, 720], 
+    video_quality = 80, 
+    video_capture_method = 'FSWEBCAM', 
     video_rate = 5)
 
 usb_cam.run()
