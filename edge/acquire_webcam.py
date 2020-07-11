e25156c8c7605c54cb1180765b1b861dbe7353f5
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-11 16:38:34 +0000

    Update acquire_webcam.py

--- edge/acquire_webcam.py
+++ edge/acquire_webcam.py
@@ -4,10 +4,12 @@ import gateway.daqc
 usb_cam = gateway.daqc.USBCam(
     channels = {160}, 
     start_delay = 10, 
-    sample_rate = 1.0, 
+    sample_rate = 1, 
     file_path = '/home/heta/Z/data/files/images/', 
     video_unit = '/dev/video0', 
     video_res = [1280, 720], 
+    video_quality = 80, 
+    video_capture_method = 'FSWEBCAM', 
     video_rate = 5)
 
 usb_cam.run()
