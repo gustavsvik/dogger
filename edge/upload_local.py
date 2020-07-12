d43abdd80e332fbdcee44ca4b53331dc44b25595
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-07-12 13:15:59 +0000

    Update upload_local.py

--- edge/upload_local.py
+++ edge/upload_local.py
@@ -2,7 +2,7 @@ import gateway.uplink
 
 
 http_upload_local = gateway.uplink.Replicate(
-    channels = {20,21,23,24,40,97,161,500}, 
+    channels = {20,21,23,24,40,97,98,161,500}, 
     start_delay = 0, 
     host_api_url = '/host/', 
     client_api_url = '/client/', 
