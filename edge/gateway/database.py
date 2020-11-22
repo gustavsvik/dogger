b15475202259ffbee8c054103ea2f3e186519426
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-22 20:02:50 +0000

    Update database.py

--- edge/gateway/database.py
+++ edge/gateway/database.py
@@ -13,7 +13,7 @@ class SQL:
     def __init__(self, gateway_database_connection = None, config_filepath = None, config_filename = None):
 
         self.gateway_database_connection = gateway_database_connection
-        
+
         self.config_filepath = config_filepath
         self.config_filename = config_filename
 
@@ -23,7 +23,7 @@ class SQL:
         self.conn = None
 
 
-    def get_env(self):
+    def get_env(self) :
 
         config = md.Configure(filepath = self.config_filepath, filename = self.config_filename)
         env = config.get()
@@ -31,29 +31,29 @@ class SQL:
         return env
 
 
-    def connect_db(self):
+    def connect_db(self) :
 
         try:
             conn_data = self.gateway_database_connection
             self.conn = pymysql.connect(host = conn_data['host'], user = conn_data['user'], passwd = conn_data['passwd'], db = conn_data['db'], autocommit = True)
-        except (pymysql.err.OperationalError, pymysql.err.Error) as e:
+        except (pymysql.err.OperationalError, pymysql.err.Error) as e :
             rt.logging.exception(e)
-            
+
         return self.conn
 
 
-    def commit_transaction(self):
+    def commit_transaction(self) :
 
         try:
             self.conn.commit()
-        except (pymysql.err.OperationalError, pymysql.err.Error) as e:
+        except (pymysql.err.OperationalError, pymysql.err.Error) as e :
             rt.logging.exception(e)
 
 
-    def close_db_connection(self):
+    def close_db_connection(self) :
 
         try:
-            if self.conn is not None: 
+            if self.conn is not None :
                 self.conn.close()
-        except (pymysql.err.OperationalError, pymysql.err.Error, NameError) as e:
+        except (pymysql.err.OperationalError, pymysql.err.Error, NameError) as e :
             rt.logging.exception(e)
