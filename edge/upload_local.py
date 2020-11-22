10c0b602dba0c292995449632ef820f53e6f493b
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-22 20:58:41 +0000

    Update upload_local.py

--- edge/upload_local.py
+++ edge/upload_local.py
@@ -5,7 +5,6 @@ http_upload_local = gateway.uplink.Replicate(
     channels = {20,21,23,24,40,97,98,161,500}, 
     start_delay = 0, 
     host_api_url = '/host/', 
-    client_api_url = '/client/', 
     max_connect_attempts = 50,
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf_local.ini')
