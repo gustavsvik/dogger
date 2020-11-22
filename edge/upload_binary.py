c8da79b129557249258e0e97aac4d596e6803682
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-22 20:59:26 +0000

    Update upload_binary.py

--- edge/upload_binary.py
+++ edge/upload_binary.py
@@ -5,7 +5,6 @@ http_upload_binary = gateway.uplink.Replicate(
     channels = {140, 160, 180, 600, 10002},
     start_delay = 0, 
     host_api_url = '/host/',
-    client_api_url = '/client/',
     max_connect_attempts = 50, 
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf.ini')
