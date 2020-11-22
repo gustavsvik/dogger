a1b81dd14c6fb149c75d1fe027a04d4d151ba14a
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-22 21:10:49 +0000

    Update manage_cloud_db_partitions.py

--- edge/manage_cloud_db_partitions.py
+++ edge/manage_cloud_db_partitions.py
@@ -5,7 +5,7 @@ partition = gateway.maint.PartitionCloudDatabase(
     start_delay = 0,
     maint_api_url = '/maint/',
     keep_partitions_horizon = 10,
-    config_filepath = '/home/heta/Z/app/python/dogger/',
+    config_filepath = '/srv/dogger/',
     config_filename = 'conf.ini')
 
 partition.run()
