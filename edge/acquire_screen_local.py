eac3b7f5803dcc4c82c8ea773301b770438f0db5
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-22 20:57:26 +0000

    Update acquire_screen_local.py

--- edge/acquire_screen_local.py
+++ edge/acquire_screen_local.py
@@ -2,11 +2,11 @@ import gateway.daqc
 
 
 screenshot = gateway.daqc.ScreenshotUpload(
-    channels = {600}, 
+    channels = {602}, 
     sample_rate = 1.0, 
     crop = [0,184,1243,1040], 
     video_quality = 80, 
-    config_filepath = 'C:\\screenshot\\',
+    config_filepath = 'Z:\\app\\python\\dogger\\',
     config_filename = 'conf_local.ini')
 
 screenshot.run()
