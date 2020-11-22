822c066602e759287f882cbaa6e5ee816c9969ea
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-22 20:05:19 +0000

    Update maint.py

--- edge/gateway/maint.py
+++ edge/gateway/maint.py
@@ -1,31 +1,182 @@
 #
 
+import sys
+import time
+import datetime
+import calendar
+import struct
+from contextlib import closing
+from socket import gaierror, socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
+import pymysql
+
+import gateway.task as t
+import gateway.uplink as ul
+import gateway.database as db
+import gateway.runtime as rt
 
 
-class NetworkTime:
+
+class PartitionDatabase(t.Maintenance) :
 
 
-    def __init__(self):
-        pass
+    def __init__(self) :
+
+        self.max_connect_attempts = 50
+
+        t.Maintenance.__init__(self)
 
 
-    def get_network_time(self):
-    
-        import datetime
-        from contextlib import closing
-        from socket import gaierror, socket, AF_INET, SOCK_DGRAM, SOCK_STREAM
-        import struct, time, sys, datetime
+    def run(self) :
 
-        NTPSERVER, PORT = 'ntp.se', 123
+        previous_monthday = -1
 
-        
+        while (True) :
+
+            time.sleep(0.5)
+
+            current_datetime = datetime.datetime.utcnow()
+            current_timestamp = current_datetime.timetuple()
+            current_year = current_timestamp.tm_year
+            current_month = current_timestamp.tm_mon
+            current_monthday = current_timestamp.tm_mday
+            current_hour =  current_timestamp.tm_hour
+            current_minute = current_timestamp.tm_min
+            current_second = current_timestamp.tm_sec
+
+            if current_minute == 50 and current_hour == 23 and current_monthday != previous_monthday :
+
+                datetime_midnight = datetime.datetime(current_year, current_month, current_monthday) + datetime.timedelta(days = 1)
+                datetime_tomorrow_midnight = datetime_midnight + datetime.timedelta(days = 1)
+                new_partition_timestamp = str(calendar.timegm(datetime_tomorrow_midnight.utctimetuple()))
+                new_partition_name_date = datetime_midnight.strftime('%Y%m%d')
+                datetime_last_midnight_to_keep = datetime_midnight - datetime.timedelta(days = self.keep_partitions_horizon)
+                oldest_kept_partition_name_date = datetime_last_midnight_to_keep.strftime('%Y%m%d')
+
+                self.partition_database(new_partition_name_date, new_partition_timestamp, oldest_kept_partition_name_date)
+
+                previous_monthday = current_monthday
+
+
+
+class PartitionEdgeDatabase(PartitionDatabase) :
+
+
+    def __init__(self, start_delay = None, ip_list = None, keep_partitions_horizon = None, config_filepath = None, config_filename = None) :
+
+        self.start_delay = start_delay
+        self.ip_list = ip_list
+        self.keep_partitions_horizon = keep_partitions_horizon
+
+        self.config_filepath = config_filepath
+        self.config_filename = config_filename
+
+        PartitionDatabase.__init__(self)
+
+        self.sql = db.SQL(config_filepath = self.config_filepath, config_filename = self.config_filename)
+
+
+    def partition_database(self, new_partition_name_date = None, new_partition_timestamp = None, oldest_kept_partition_name_date = None) :
+
+        try:
+
+            self.sql.connect_db()
+            db_name = self.sql.gateway_database_connection['db']
+
+            sql_reorganize_partitions = "ALTER TABLE " + db_name + ".t_acquired_data REORGANIZE PARTITION acquired_time_max INTO ( PARTITION acquired_time_" + new_partition_name_date + " VALUES LESS THAN (" + new_partition_timestamp + "), PARTITION acquired_time_max VALUES LESS THAN (MAXVALUE) )"
+            print(sql_reorganize_partitions)
+            with self.sql.conn.cursor() as cursor :
+                try:
+                    cursor.execute(sql_reorganize_partitions)
+                except pymysql.err.InternalError as e:
+                    print(e)
+
+            sql_get_all_date_partition_strings = "SELECT PARTITION_NAME FROM INFORMATION_SCHEMA.PARTITIONS WHERE TABLE_SCHEMA='" + db_name + "' AND PARTITION_NAME IS NOT NULL AND LOWER(PARTITION_NAME)<>'acquired_time_max' ORDER BY PARTITION_NAME"
+            print(sql_get_all_date_partition_strings)
+            all_partition_date_strings = []
+            with self.sql.conn.cursor() as cursor :
+                try:
+                    cursor.execute(sql_get_all_date_partition_strings)
+                except pymysql.err.InternalError as e:
+                    print(e)
+                results = cursor.fetchall()
+                for row in results:
+                    date_partition_string = row[0]
+                    all_partition_date_strings.append(date_partition_string[-8:])
+
+            for partition_date_string in all_partition_date_strings :
+                if partition_date_string < oldest_kept_partition_name_date :
+                    sql_drop_old_partition = "ALTER TABLE " + db_name + ".t_acquired_data DROP PARTITION acquired_time_" + partition_date_string
+                    print(sql_drop_old_partition)
+                    with self.sql.conn.cursor() as cursor :
+                        try:
+                            cursor.execute(sql_drop_old_partition)
+                        except pymysql.err.InternalError as e:
+                            print(e)
+
+        except (pymysql.err.OperationalError, pymysql.err.Error) as e:
+            print(e)
+
+        finally:
+            try:
+                self.sql.close_db_connection()
+            except pymysql.err.Error as e:
+                print(e)
+
+
+
+class PartitionCloudDatabase(PartitionDatabase) :
+
+
+    def __init__(self, start_delay = None, ip_list = None, maint_api_url = None, keep_partitions_horizon = None, config_filepath = None, config_filename = None) :
+
+        self.start_delay = start_delay
+        self.ip_list = ip_list
+        self.maint_api_url = maint_api_url
+        self.keep_partitions_horizon = keep_partitions_horizon
+
+        self.config_filepath = config_filepath
+        self.config_filename = config_filename
+
+        PartitionDatabase.__init__(self)
+
+
+    def partition_database(self, new_partition_name_date = None, new_partition_timestamp = None, oldest_kept_partition_name_date = None) :
+
+        print(self.start_delay, self.ip_list, self.maint_api_url, new_partition_name_date, new_partition_timestamp, oldest_kept_partition_name_date)
+        http = ul.CloudDBPartition(start_delay = self.start_delay, ip_list = self.ip_list, maint_api_url = self.maint_api_url, max_connect_attempts = self.max_connect_attempts, new_partition_name_date = new_partition_name_date, new_partition_timestamp = new_partition_timestamp, oldest_kept_partition_name_date = oldest_kept_partition_name_date)
+
+        if self.ip_list is not None :
+            for current_ip in self.ip_list :
+                print("current_ip", current_ip)
+                res = http.partition_database(current_ip)
+
+
+
+class NetworkTime(t.Maintenance) :
+
+
+    def __init__(self, start_delay = None, ntp_url = None, ntp_port = None, adjust_interval = None, config_filepath = None, config_filename = None) :
+
+        self.start_delay = start_delay
+        self.ntp_url = ntp_url
+        self.ntp_port = ntp_port
+        self.adjust_interval = adjust_interval
+
+        self.config_filepath = config_filepath
+        self.config_filename = config_filename
+
+        t.Maintenance.__init__(self)
+
+
+    def get_network_time(self) :
+
         try :
 
-            with closing(socket(AF_INET, SOCK_DGRAM)) as s:
-            
+            with closing(socket(AF_INET, SOCK_DGRAM)) as s :
+
                 t0 = time.time()
 
-                s.sendto(('\x23' + 47 * '\0').encode(), (NTPSERVER, PORT))   # see https://stackoverflow.com/a/26938508
+                s.sendto(('\x23' + 47 * '\0').encode(), (self.ntp_url, self.ntp_port))   # see https://stackoverflow.com/a/26938508
                 msg, address = s.recvfrom(1024)                   # and https://stackoverflow.com/a/33436061
                 unpacked = struct.unpack("!12I", msg[0:struct.calcsize("!12I")])  # ! => network (= big-endian), 12 => returns 12-tuple, I => unsigned int
                 t3 = time.time()
@@ -41,13 +192,13 @@ class NetworkTime:
                 print("Offset                                                 %.1f ms" % (offset * 1000) )
                 print("Local -> NTP server -> local roundtrip time            %.1f ms" % (roundtrip * 1000) )
                 print("New local computer time                                %.3f" % (t3 + offset) )
-        
+
                 self.current_system_time = t3
                 self.current_system_time_offset = offset
 
         except gaierror as e :
             print(e)
-        
+
         dt = None
         try :
             dt = datetime.datetime.utcfromtimestamp(self.current_system_time + self.current_system_time_offset)
@@ -57,23 +208,23 @@ class NetworkTime:
         return dt
 
 
-    def adjust_system_time(self, dt):
+    def adjust_win_system_time(self, dt) :
 
         import win32api  # pip install pywin32
 
         win32api.SetSystemTime(dt.year, dt.month, dt.isocalendar()[2], dt.day, dt.hour, dt.minute, dt.second, int(dt.microsecond / 1000) )
 
-        
-    def run_continuous_adjustment(self, secs_interval):
-    
+
+    def run(self) :
+
         import time, numpy
 
         adj_time = self.get_network_time()
         if adj_time is not None :
-            self.adjust_system_time(adj_time)
+            self.adjust_win_system_time(adj_time)
 
         acq_prev_time = numpy.float64(time.time())
-        
+
         diff_time = 0
 
         while True :
@@ -82,8 +233,8 @@ class NetworkTime:
             diff_time += acq_finish_time - acq_prev_time
             acq_prev_time = acq_finish_time
 
-            if diff_time > secs_interval :
+            if diff_time > self.adjust_interval :
                 adj_time = self.get_network_time()
                 if adj_time is not None :
-                    self.adjust_system_time(adj_time)
+                    self.adjust_win_system_time(adj_time)
                     diff_time = 0
