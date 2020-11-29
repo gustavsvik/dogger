e6c1a9f089e630f7550827267ca1f035876621d8
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-29 11:26:44 +0000

    Update utils.py

--- edge/gateway/utils.py
+++ edge/gateway/utils.py
@@ -1,3 +1,5 @@
+import json
+import yaml
 
 
 def nmea_checksum(nmea_data) :
@@ -14,3 +16,31 @@ def nmea_checksum(nmea_data) :
 def get_channel_range_string(channels) :
 
     return ';;'.join([str(ch) for ch in channels]) + ';;'
+
+
+def instance_from_dict(cls, argument_dict) :
+
+    return cls(**argument_dict)
+
+
+def instance_from_dict_string(cls, dict_string) :
+
+    return instance_from_dict(cls, eval(dict_string))
+
+
+def instance_from_argument_string(cls, argument_string) :
+
+    return instance_from_dict_string(cls, "dict({})".format(argument_string))
+
+
+def instance_from_json_string(cls, json_string) :
+
+    json_object = json.loads(json_string)
+    return instance_from_dict(cls, dict(json_object))
+
+    
+def instance_from_yaml_string(cls, yaml_string) :
+
+    yaml_object = yaml.safe_load(yaml_string)
+    print(dict(yaml_object))
+    return instance_from_dict(cls, dict(yaml_object))
