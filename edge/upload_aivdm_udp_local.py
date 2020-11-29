df10e2d35a1f36fc024e6cfd0383323500ae086b
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-11-29 18:49:50 +0000

    Update upload_aivdm_udp_local.py

--- edge/upload_aivdm_udp_local.py
+++ edge/upload_aivdm_udp_local.py
@@ -1,12 +1,15 @@
 import gateway.uplink
 
 
-udp_upload_ais = gateway.uplink.UdpAis(
+udp_upload_ais = gateway.uplink.UdpAivdmPosition(
     channels = {61011,61012}, 
     start_delay = 0,
     port = 4444,
     max_age = 10,
     mmsi = 123456789,
+    vessel_name = "L01 VW CRAFTER", 
+    call_sign = "FJJ976", 
+    ship_type = 90,
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf_udp_local.ini')
 
