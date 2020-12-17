6ed9173ffa9fa08217552914cc566f4eb856d449
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-17 13:59:41 +0000

    Update store_binary.py

--- edge/store_binary.py
+++ edge/store_binary.py
@@ -5,6 +5,7 @@ image_sql = gateway.store.ImageFile(
     channels = {140,141,160,161,180,181}, 
     start_delay = 0, 
     file_path = '/home/heta/Z/data/files/images/',
+    files_to_keep = 0,
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf.ini')
 
