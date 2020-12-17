d6764e74068906894bc5a2a7fd567b0e0709e464
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-17 14:47:37 +0000

    Update acquire_nmea_udp.py

--- edge/acquire_nmea_udp.py
+++ edge/acquire_nmea_udp.py
@@ -1,7 +1,7 @@
 import gateway.daqc
 
 
-acquire_nmea = gateway.daqc.PosNmeaUdpFile(
+mob_acquire_nmea_udp_file = gateway.daqc.PosNmeaUdpFile(
     channels = {61010,61011,61012}, 
     port = 50000,
     start_delay = 0,
@@ -11,4 +11,4 @@ acquire_nmea = gateway.daqc.PosNmeaUdpFile(
     config_filepath = '/srv/dogger/', 
     config_filename = 'conf.ini')
 
-acquire_nmea.run()
+mob_acquire_nmea_udp_file.run()
