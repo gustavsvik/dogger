738b29ee250a67a5d6ddf0f94214b351dcaf942d
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-17 13:58:47 +0000

    Update store_screen.py

--- edge/store_screen.py
+++ edge/store_screen.py
@@ -5,6 +5,7 @@ image_sql = gateway.store.ScreenshotFile(
     channels = {600}, 
     start_delay = 0, 
     file_path = '/home/heta/Z/data/files/screenshots/',
+    files_to_keep = 0,
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf.ini')
 
