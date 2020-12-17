88ed3d6718fbf83415c3ef8a3fa105dcfe7a6579
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-17 14:53:53 +0000

    Add new file

---
+++ edge/upload_aivdm_area_notice_circle_udp_local.py
@@ -0,0 +1,13 @@
+import gateway.link
+
+
+udp_upload_ais = gateway.link.SqlUdpBinaryBroadcastMessageAreaNoticeCircle(
+    channels = {61011,61012}, 
+    start_delay = 0,
+    port = 4444,
+    max_age = 10,
+    mmsi = 123456789,
+    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    config_filename = 'conf_udp_local.ini')
+
+udp_upload_ais.run()
