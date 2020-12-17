17b6e7d05614241bc35bcc0704270b29cdc47045
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-17 13:59:18 +0000

    Update store_current.py

--- edge/store_current.py
+++ edge/store_current.py
@@ -5,6 +5,7 @@ numpy_sql = gateway.store.NumpyFile(
     channels = {97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112}, 
     start_delay = 0, 
     file_path = '/home/heta/Z/data/files/current/',
+    files_to_keep = 0,
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf.ini')
 
