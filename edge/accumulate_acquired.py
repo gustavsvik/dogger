7701ab80b79e774284d6f641db0da8a8d7f2f53e
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-17 13:56:30 +0000

    Update accumulate_acquired.py

--- edge/accumulate_acquired.py
+++ edge/accumulate_acquired.py
@@ -1,7 +1,7 @@
-import gateway.store
+import gateway.transform
 
 
-accumulate_acquired = gateway.store.Accumulate(
+accumulate_acquired = gateway.transform.Accumulate(
     channels = {20,21,22,23,24,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112}, 
     start_delay = 20, 
     config_filepath = '/home/heta/Z/app/python/dogger/', 
