ca398ce6c2b4accb5e54d2c7d2b8c056427f03dd
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-17 13:56:02 +0000

    Update upload_udp.py

--- edge/upload_udp.py
+++ edge/upload_udp.py
@@ -1,12 +1,12 @@
-import gateway.uplink
+import gateway.link
 
 
-udp_upload = gateway.uplink.UdpRaw(
-    channels = {61011,61012}, 
+mob_upload_sql_udp = gateway.link.SqlUdpRaw(
+    channels = {142,98}, 
     start_delay = 0,
     port = 61010,
     max_age = 10,
-    config_filepath = '/srv/dogger/', 
+    config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf.ini')
 
-udp_upload.run()
+mob_upload_sql_udp.run()
