6245360b9955e75b1e03d99b60a370559e11b48b
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-31 11:48:57 +0000

    Add new file

---
+++ edge/upload_radar_mockup_1.py
@@ -0,0 +1,13 @@
+import gateway.daqc
+
+
+screenshot = gateway.daqc.TempFileUpload(
+    channels = {2002}, 
+    sample_rate = 1.0, 
+    host_api_url = '/host/',
+    client_api_url = '/client/',
+    file_extension = 'png',
+    config_filepath = '/srv/dogger/', 
+    config_filename = 'conf.ini')
+
+screenshot.run()
