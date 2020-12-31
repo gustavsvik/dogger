89ec309e4c9166c41262e56d1983bf63b304bf90
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-31 11:52:25 +0000

    Update store_text_string.py

--- edge/store_text_string.py
+++ edge/store_text_string.py
@@ -5,6 +5,7 @@ mob_store_text_sql = gateway.store.TextStringFile(
     channels = {61010}, 
     start_delay = 0, 
     file_path = '/srv/dogger/files/',
+    files_to_keep = 0,
     config_filepath = '/srv/dogger/', 
     config_filename = 'conf.ini')
 
