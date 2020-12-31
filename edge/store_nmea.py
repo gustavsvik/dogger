cad538f7296527f1f508fc8f651e9a25f3225097
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-31 11:53:16 +0000

    Update store_nmea.py

--- edge/store_nmea.py
+++ edge/store_nmea.py
@@ -5,6 +5,7 @@ mob_store_numpy_sql = gateway.store.NumpyFile(
     channels = {61011,61012}, 
     start_delay = 0, 
     file_path = '/srv/dogger/files/',
+    files_to_keep = 0,
     config_filepath = '/srv/dogger/', 
     config_filename = 'conf.ini')
 
