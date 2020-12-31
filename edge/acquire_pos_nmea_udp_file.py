7c273c774db9e86a2c40db29b9fc3ed8a05af82a
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2020-12-31 11:47:40 +0000

    Update acquire_pos_nmea_udp_file.py

--- edge/acquire_pos_nmea_udp_file.py
+++ edge/acquire_pos_nmea_udp_file.py
@@ -1,13 +1,14 @@
 import gateway.daqc
 
 
-mob_acquire_nmea_udp_file = gateway.daqc.PosNmeaUdpFile(
-    channels = {61010,61011,61012}, 
+mob_acquire_nmea_udp_file = gateway.daqc.NmeaUdpFile(
+    channels = {'GGA':{61010:'txt',61011:'npy',61012:'npy'}, 'VTG':{61020:'txt'}}, 
     port = 50000,
     start_delay = 0,
     sample_rate = 1.0,
     file_path = '/srv/dogger/files/',
     archive_file_path = '/srv/dogger/files/',
+    line_identifier = 'GGA',
     config_filepath = '/srv/dogger/', 
     config_filename = 'conf.ini')
 
