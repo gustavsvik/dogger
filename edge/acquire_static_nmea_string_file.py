0ea2f93786c9ab0321675fa9faf48028ea20f5e1
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-03 14:12:50 +0000

    Add new file

---
+++ edge/acquire_static_nmea_string_file.py
@@ -0,0 +1,13 @@
+import gateway.daqc
+
+
+acquire_static_file = gateway.daqc.StaticFileNmeaFile(
+    channels = { 'TTM':{162:'txt'} }, 
+    start_delay = 0, 
+    sample_rate = 0.5, 
+    static_file_path_name = '/home/heta/Z/data/arpa_nmea.txt',
+    file_path = '/home/heta/Z/data/files/', 
+    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    config_filename = 'conf.ini')
+
+acquire_static_file.run()
