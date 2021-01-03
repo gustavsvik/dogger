50eb6b794e894e74ba23e286c5d47d7efb94701d
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-03 14:15:26 +0000

    Update upload_udp_cloud.py

--- edge/upload_udp_cloud.py
+++ edge/upload_udp_cloud.py
@@ -1,7 +1,7 @@
 import gateway.link
 
 
-mob_upload_sql_udp = gateway.link.SqlUdpRaw(
+mob_upload_sql_udp = gateway.link.SqlUdpRawValue(
     channels = {61011,61012}, 
     start_delay = 0,
     port = 61010,
