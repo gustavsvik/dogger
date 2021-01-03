c456605ccb1ca1958e80141be9561d44c0586e68
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-03 14:07:27 +0000

    Add new file

---
+++ edge/upload_udp_bytes.py
@@ -0,0 +1,12 @@
+import gateway.link
+
+
+mob_upload_sql_udp = gateway.link.SqlUdpRawBytes(
+    channels = {144,145,162}, 
+    start_delay = 0,
+    port = 61011,
+    max_age = 10,
+    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    config_filename = 'conf.ini')
+
+mob_upload_sql_udp.run()
