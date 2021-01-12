bd103fc9f408d8286cf960f6a5c62166e244a296
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-12 02:02:32 +0000

    Update upload_ais_udp_local.py

--- edge/upload_ais_udp_local.py
+++ edge/upload_ais_udp_local.py
@@ -1,9 +1,10 @@
 import gateway.link
 
 
-udp_upload_local = gateway.link.SqlUdpBytes(
-    channels = {144,145,162}, 
+udp_upload_local = gateway.link.SqlUdpNmeaLines(
+    channels = {144,145}, 
     start_delay = 0,
+    transmit_rate = 1, 
     port = 4444,
     max_age = 10,
     config_filepath = '/home/heta/Z/app/python/dogger/', 
