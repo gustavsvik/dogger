902150306b200e75e3ede610d1f7cc04f297f634
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-12 02:06:43 +0000

    Update upload_binary_local.py

--- edge/upload_binary_local.py
+++ edge/upload_binary_local.py
@@ -4,6 +4,7 @@ import gateway.link
 http_upload_binary_local = gateway.link.Replicate(
     channels = {140,160,180,600,10002}, 
     start_delay = 0, 
+    transmit_rate = 1, 
     host_api_url = '/host/', 
     max_connect_attempts = 50,
     config_filepath = '/home/heta/Z/app/python/dogger/', 
