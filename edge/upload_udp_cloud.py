f359d5f3bf9a22c1f836d35a055457c536969c94
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-12 02:04:29 +0000

    Update upload_udp_cloud.py

--- edge/upload_udp_cloud.py
+++ edge/upload_udp_cloud.py
@@ -4,6 +4,7 @@ import gateway.link
 mob_upload_sql_udp = gateway.link.SqlUdpRawValue(
     channels = {61011,61012}, 
     start_delay = 0,
+    transmit_rate = 1, 
     port = 61010,
     max_age = 10,
     config_filepath = '/srv/dogger/', 
