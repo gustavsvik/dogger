15d8e2cb25275e8b846114fa024f0a37375d5ff9
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-12 02:06:06 +0000

    Update upload_pos_udp_local.py

--- edge/upload_pos_udp_local.py
+++ edge/upload_pos_udp_local.py
@@ -4,6 +4,7 @@ import gateway.link
 udp_upload_pos_local = gateway.link.SqlUdpNmeaPos(
     channels = {61011,61012}, 
     start_delay = 0,
+    transmit_rate = 1, 
     port = 4444,
     nmea_prepend = 'GPGLL,',
     nmea_append = ',A',
