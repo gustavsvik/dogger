d64d086b711dee5bfa234b950c101fc1372d7583
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-12 02:08:42 +0000

    Update upload_aivdm_udp_local.py

--- edge/upload_aivdm_udp_local.py
+++ edge/upload_aivdm_udp_local.py
@@ -4,6 +4,7 @@ import gateway.link
 udp_upload_ais = gateway.link.SqlUdpAivdmPosition(
     channels = {61011,61012}, 
     start_delay = 0,
+    transmit_rate = 1, 
     port = 4444,
     max_age = 10,
     mmsi = 123456789,
