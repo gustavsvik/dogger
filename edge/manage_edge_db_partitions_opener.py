4e0aad8130369eab343908691ed569b8711cda02
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-12 01:55:51 +0000

    Add new file

---
+++ edge/manage_edge_db_partitions_opener.py
@@ -0,0 +1,10 @@
+import gateway.maint
+
+
+partition = gateway.maint.PartitionEdgeDatabase(
+    start_delay = 0,
+    keep_partitions_horizon = 10,
+    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    config_filename = 'conf_opener.ini')
+
+partition.run()
