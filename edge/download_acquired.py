900686bf9cd5434820ff57d6d7f5615baebd5ccd
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-12 01:56:32 +0000

    Update download_acquired.py

--- edge/download_acquired.py
+++ edge/download_acquired.py
@@ -2,11 +2,12 @@ import gateway.link
 
 
 http_download = gateway.link.HttpSql(
-    channels = {61011,61012,143,98,144,145,162}, 
+    channels = {61011,61012,143,97,98,144,145,162}, 
     start_delay = 0, 
+    transmit_rate = 1, 
     client_api_url = '/client/', 
     max_connect_attempts = 50, 
     config_filepath = '/home/heta/Z/app/python/dogger/', 
-    config_filename = 'conf.ini')
+    config_filename = 'conf_opener.ini')
 
 http_download.run()
