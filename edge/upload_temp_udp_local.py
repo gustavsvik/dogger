0348d6c10a2f24729d38f734dfe3916c1d0f336c
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-01-12 02:05:35 +0000

    Update upload_temp_udp_local.py

--- edge/upload_temp_udp_local.py
+++ edge/upload_temp_udp_local.py
@@ -4,6 +4,7 @@ import gateway.link
 udp_upload_local = gateway.link.SqlUdpNmeaValue(
     channels = {98}, 
     start_delay = 0,
+    transmit_rate = 1, 
     port = 4444,
     multiplier = 1.0,
     decimals = 2,
