4c647ca68503c8e14e36118a3b9cf884310ea2bc
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-02-13 19:12:07 +0000

    Update acquire_udp_bytes_http.py

--- edge/acquire_udp_bytes_http.py
+++ edge/acquire_udp_bytes_http.py
@@ -2,7 +2,10 @@ import gateway.daqc
 
 
 sie_acquire_udp_http = gateway.daqc.UdpBytesHttp(
+    channels = set(), 
+    start_delay = 0, 
     port = 61011,
+    max_connect_attempts = 50, 
     config_filepath = '/srv/dogger/', 
     config_filename = 'conf.ini')
 
