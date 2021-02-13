f80337b90d10f998811fa0896beecf3514a5c662
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-02-13 18:56:25 +0000

    Update upload_aivdm_pos_aton_udp_local.py

--- edge/upload_aivdm_pos_aton_udp_local.py
+++ edge/upload_aivdm_pos_aton_udp_local.py
@@ -2,17 +2,17 @@ import gateway.link
 
 
 udp_upload_ais = gateway.link.SqlUdpAtonReport(
-    channels = {61011,61012}, 
+    channels = {168,169}, 
     start_delay = 0,
-    transmit_rate = 1, 
+    transmit_rate = 0.5, 
     port = 4444,
     max_age = 10,
-    length_offset = [50, -20, -20, 50, 30],
-    width_offset = [20, 20, -20, -20, 0],
-    mmsi = [992659999, 992659998, 992659997, 992659996, 992659995],
-    aid_type = [30, 30, 30, 30, 28],
-    name = ["TEST AREA DEMARC. 1", "TEST AREA DEMARC. 2", "TEST AREA DEMARC. 3", "TEST AREA DEMARC. 4", "MEASUREMENT BUOY"],
-    virtual_aid = [1, 1, 1, 1, 0],
+    length_offset = [-400, -160, 180, 500],
+    width_offset = [-300, -50, 170, 270],
+    mmsi = [992659999, 992659998, 992659997, 992659996],
+    aid_type = [19, 30, 30, 19],
+    name = ["MEASUREMENT BEACON", "TEST AREA NO PASSAGE", "TEST AREA NO PASSAGE", "MEASUREMENT BEACON"],
+    virtual_aid = [0, 1, 1, 0],
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf_udp_local.ini')
 
