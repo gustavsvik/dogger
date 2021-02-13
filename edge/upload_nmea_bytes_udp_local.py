8dd6b098f12faafb8706289ddaf1bc91e5a42100
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-02-13 19:18:07 +0000

    Add new file

---
+++ edge/upload_nmea_bytes_udp_local.py
@@ -0,0 +1,13 @@
+import gateway.link
+
+
+udp_upload_local = gateway.link.SqlUdpBytes(
+    channels = {162,163}, 
+    start_delay = 0,
+    transmit_rate = 1, 
+    port = 4444,
+    max_age = 10,
+    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    config_filename = 'conf_udp_local.ini')
+
+udp_upload_local.run()
