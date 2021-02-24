ade9c54c1deb8cf538b450645c274be22cdf96b5
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-02-24 06:33:40 +0000

    Update upload_aivdm_pos_aton_file.py

--- edge/upload_aivdm_pos_aton_file.py
+++ edge/upload_aivdm_pos_aton_file.py
@@ -6,14 +6,15 @@ udp_upload_ais = gateway.link.SqlFileAtonReport(
     start_delay = 0,
     transmit_rate = 0.5, 
     max_age = 10,
-    target_channels = {170},
+    target_channels = {'VDM':{170:'txt'}},
     length_offset = [-400, -160, 180, 500],
     width_offset = [-300, -50, 170, 270],
     mmsi = [992659999, 992659998, 992659997, 992659996],
     aid_type = [19, 30, 30, 19],
     name = ["MEASUREMENT BEACON", "TEST AREA NO PASSAGE", "TEST AREA NO PASSAGE", "MEASUREMENT BEACON"],
     virtual_aid = [0, 1, 1, 0],
-    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    file_path = '/srv/dogger/files/',
+    config_filepath = '/srv/dogger/', 
     config_filename = 'conf.ini')
 
 udp_upload_ais.run()
