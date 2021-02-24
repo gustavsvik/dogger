ba18b78759e4ac499e979cc65c402d8be39c1d93
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-02-24 06:23:26 +0000

    Update store_text_string.py

--- edge/store_text_string.py
+++ edge/store_text_string.py
@@ -2,7 +2,7 @@ import gateway.store
 
 
 mob_store_text_sql = gateway.store.TextStringFile(
-    channels = {142,144,145,150,151,162,163,167},
+    channels = {142,144,145,150,151,162,163,167,171},
     start_delay = 0, 
     file_path = '/home/heta/Z/data/files/', 
     files_to_keep = 0,
