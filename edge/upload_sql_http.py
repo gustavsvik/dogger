1d01059b8a45441e38f1f89147328e51ae8c6637
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-02-24 06:29:52 +0000

    Add new file

---
+++ edge/upload_sql_http.py
@@ -0,0 +1,13 @@
+import gateway.link
+
+
+sie_upload_sql_http = gateway.link.SqlHttp(
+    channels = {168,169,170}, 
+    start_delay = 0, 
+    transmit_rate = 0.5,
+    max_age = 10,
+    max_connect_attempts = 50, 
+    config_filepath = '/srv/dogger/', 
+    config_filename = 'conf.ini')
+
+sie_upload_sql_http.run()
