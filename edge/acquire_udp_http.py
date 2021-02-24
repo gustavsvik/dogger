9e135f6f0345ffb5cc59914554607f7b2b03a895
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-02-24 06:31:30 +0000

    Update acquire_udp_http.py

--- edge/acquire_udp_http.py
+++ edge/acquire_udp_http.py
@@ -4,6 +4,7 @@ import gateway.daqc
 sie_acquire_udp_http = gateway.daqc.UdpValueHttp(
     channels = set(), 
     start_delay = 0, 
+    transmit_rate = 10,
     port = 61010,
     max_connect_attempts = 50, 
     config_filepath = '/srv/dogger/', 
