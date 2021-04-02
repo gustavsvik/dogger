0a74badb72befb5e248f11f69055179af65eb282
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-04-02 10:37:30 +0000

    Update transform.py

--- edge/gateway/transform.py
+++ edge/gateway/transform.py
@@ -4,7 +4,10 @@ import time
 import datetime
 import math
 import numpy
-
+try:
+    import pyais
+except ImportError:
+    pass
 import gateway.runtime as rt
 import gateway.aislib as ai
 
@@ -13,11 +16,11 @@ import gateway.aislib as ai
 def channel_value_dict(channels_list, values_list) :
 
     channel_value_tuple_list = list(zip(channels_list, values_list))
-    print("channel_value_tuple_list", channel_value_tuple_list)
+    rt.logging.debug("channel_value_tuple_list", channel_value_tuple_list)
     min_common_length = min( len(channels_list), len(values_list) )
-    print("min_common_length", min_common_length)
+    rt.logging.debug("min_common_length", min_common_length)
     data_dict = dict(channel_value_tuple_list)
-    print("data_dict", data_dict)
+    rt.logging.debug("data_dict", data_dict)
     return data_dict
 
 
@@ -89,6 +92,21 @@ def de_armor_separators(byte_string = None) :
     return replaced_byte_string
 
 
+def data_field_binary_string(message_data_binary_string, start_in_complete_message, field_start, field_end) :
+
+    message_data_start_index = field_start - start_in_complete_message
+    message_data_end_index = field_end - start_in_complete_message + 1
+    field_binary_string = message_data_binary_string[ message_data_start_index : message_data_end_index ]
+    return field_binary_string
+
+
+def twos_comp(val, bits):
+    """compute the 2's complement of int value val"""
+    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+        val = val - (1 << bits)        # compute negative value
+    return val                         # return positive value as is
+
+
 
 class TextNumeric :
 
@@ -103,7 +121,7 @@ class TextNumeric :
 
         if time_tuple is None :
             time_tuple = time.gmtime(time.time())
-            print("time_tuple", time_tuple)
+            rt.logging.debug("time_tuple", time_tuple)
 
         string_dict = {}
 
@@ -127,12 +145,12 @@ class TextNumeric :
                         string_dict[selected_tag] = current_string_value + selected_line
                         if line_end is not None :
                             string_dict[selected_tag] += line_end
-        print("string_dict", string_dict)
+        rt.logging.debug("string_dict", string_dict)
 
         data_arrays = []
 
         for selected_tag, channels in channel_data.items() :
-            print("channel_data", channel_data, "selected_tag", selected_tag, "channels", channels)
+            rt.logging.debug("channel_data", channel_data, "selected_tag", selected_tag, "channels", channels)
             channels_list = sorted(list(channels))
             rt.logging.debug("channels_list", channels_list)
 
@@ -141,7 +159,7 @@ class TextNumeric :
                 dict_string = string_dict[selected_tag] #+ '\r\n'
             except KeyError as e :
                 pass
-            print("dict_string", dict_string)
+            rt.logging.debug("dict_string", dict_string)
 
             if line_end is not None :
                 if dict_string.endswith(line_end) :
@@ -172,15 +190,15 @@ class TextNumeric :
                     data_array = [ dict( [ (channels_list[0], dict_string) ] ) ]
 
                 if selected_tag == 'GGA' :
-                    print("channels_list", channels_list, "dict_string", dict_string, "time_tuple", time_tuple)
+                    rt.logging.debug("channels_list", channels_list, "dict_string", dict_string, "time_tuple", time_tuple)
                     validated_timestamp, timestamp_microsecs, latitude, longitude = self.gga_to_time_pos(dict_string, time_tuple)
-                    print("[latitude]", [latitude], "[longitude]", [longitude])
+                    rt.logging.debug("[latitude]", [latitude], "[longitude]", [longitude])
                     data_dict = None
                     if not ( None in [latitude, longitude] ) :
                         gga_string_valid_time = self.gga_from_time_pos_float(validated_timestamp, latitude, longitude)
-                        print("gga_string_valid_time", gga_string_valid_time)
+                        rt.logging.debug("gga_string_valid_time", gga_string_valid_time)
                         data_dict = channel_value_dict(channels_list, [ gga_string_valid_time, [latitude], [longitude] ])
-                    print("data_dict", data_dict)
+                    rt.logging.debug("data_dict", data_dict)
                     data_array = [data_dict]
 
                 if selected_tag == 'GLL' :
@@ -199,7 +217,7 @@ class TextNumeric :
                 if len(data_array) > 0 :
                     data_arrays.append( dict( [ (selected_tag, data_array) ] ) )
 
-        print("data_arrays", data_arrays)
+        rt.logging.debug("data_arrays", data_arrays)
         return data_arrays
 
 
@@ -368,7 +386,7 @@ class Nmea(TextNumeric) :
             nmea_data += self.append
 
         nmea_string = '$' + nmea_data + '*' + self.get_checksum(nmea_data) + '\n'
-        print('nmea_string', nmea_string)
+        rt.logging.debug('nmea_string', nmea_string)
 
         return nmea_string
 
@@ -377,7 +395,7 @@ class Nmea(TextNumeric) :
 
         #rt.logging.debug("nmea_string", nmea_string)
         nmea_fields = nmea_string.split(',')
-        print("nmea_fields", nmea_fields)
+        rt.logging.debug("nmea_fields", nmea_fields)
 
         year = current_timetuple[0]
         month = current_timetuple[1]
@@ -409,12 +427,12 @@ class Nmea(TextNumeric) :
         latitude = None
         longitude = None
         try :
-            print("aivdo_string", aivdo_string)
+            rt.logging.debug("aivdo_string", aivdo_string)
             aivdo_data = aivdo_instance.decode(aivdo_string, ignore_crc = True)
             rt.logging.debug("aivdo_data.mmsi.int", aivdo_data.mmsi.int) 
             longitude = aivdo_data.lon.int / 10000 / 60 
             latitude = aivdo_data.lat.int / 10000 / 60 
-        except (KeyError, UnboundLocalError) as e :
+        except (KeyError, ValueError, UnboundLocalError) as e :
             rt.logging.exception(e)
 
         return latitude, latitude
@@ -425,14 +443,14 @@ class Nmea(TextNumeric) :
         aivdm_payload = self.prepend
 
         try :
-            print(mmsi, call_sign, vessel_name, ship_type)
+            rt.logging.debug(mmsi, call_sign, vessel_name, ship_type)
             aivdm_message = ai.AISStaticAndVoyageReportMessage(mmsi = mmsi, callsign = call_sign, shipname = vessel_name, shiptype = ship_type, imo = 0)
             aivdm_instance = ai.AIS(aivdm_message)
             aivdm_payload += aivdm_instance.build_payload(False)
-            print("aivdm_payload", aivdm_payload)
+            rt.logging.debug("aivdm_payload", aivdm_payload)
         except ValueError as e :
             aivdm_payload += ''
-            print(e)
+            rt.logging.exception(e)
         finally :
             aivdm_payload += self.append
 
@@ -521,12 +539,12 @@ class Nmea(TextNumeric) :
 
                 latitude_degs += float ( length_offset / ( 1 * 1852 ) * 1/60 )
                 latitude_min_fraction = int(latitude_degs * 60 * 10000)
-                print("latitude_degs", latitude_degs, "latitude_min_fraction", latitude_min_fraction)
+                rt.logging.debug("latitude_degs", latitude_degs, "latitude_min_fraction", latitude_min_fraction)
 
                 latitude_factor = math.cos(latitude_degs/180 * math.pi)
                 longitude_degs += float ( width_offset / ( latitude_factor * 1852 ) * 1/60 )
                 longitude_min_fraction = int(longitude_degs * 60 * 10000)
-                print("longitude_degs", longitude_degs, "longitude_min_fraction", longitude_min_fraction)
+                rt.logging.debug("longitude_degs", longitude_degs, "longitude_min_fraction", longitude_min_fraction)
 
                 rt.logging.debug("mmsi", mmsi, "aid_type", aid_type, "name", name, "longitude_min_fraction", longitude_min_fraction, "latitude_min_fraction", latitude_min_fraction, "virtual_aid", virtual_aid)
                 aivdm_message = ai.AISAtonReport(mmsi = mmsi, aid_type = aid_type, name = name, lon = longitude_min_fraction, lat = latitude_min_fraction, virtual_aid = virtual_aid)
@@ -545,3 +563,73 @@ class Nmea(TextNumeric) :
             pass
 
         return aivdm_payloads
+
+
+    def data_from_aivdm(self, aivdm_message_strings) :
+
+        mmsis = []
+        latitudes = []
+        longitudes = []
+
+        #rt.logging.debug("aivdm_message_strings", aivdm_message_strings)
+
+        for aivdm_message_string in aivdm_message_strings :
+            aivdm_messages = aivdm_message_string.split(b' ')
+            for aivdm_message in aivdm_messages :
+                aivdm_data = []
+                try :
+                    aivdm_data = pyais.NMEAMessage(aivdm_message).decode()
+                except (ValueError, IndexError, pyais.exceptions.InvalidNMEAMessageException) as e :
+                    pass #rt.logging.exception(e)
+                try :
+                    mmsi = aivdm_data['mmsi']
+                    if int(aivdm_data['type']) == 8 and int(aivdm_data['fid']) == 31 :
+                        lon = int( data_field_binary_string(aivdm_data['data'], 56, 56, 80), 2 ) / 1000 / 60
+                        lat = int( data_field_binary_string(aivdm_data['data'], 56, 81, 104), 2 ) / 1000 / 60
+                        accuracy = bool( data_field_binary_string(aivdm_data['data'], 56, 105, 105) )
+                        day = int( data_field_binary_string(aivdm_data['data'], 56, 106, 110), 2 )
+                        hour = int( data_field_binary_string(aivdm_data['data'], 56, 111, 115), 2 )
+                        minute = int( data_field_binary_string(aivdm_data['data'], 56, 116, 121), 2 )
+                        wspeed = int( data_field_binary_string(aivdm_data['data'], 56, 122, 128), 2 )
+                        wgust = int( data_field_binary_string(aivdm_data['data'], 56, 129, 135), 2 )
+                        wdir = int( data_field_binary_string(aivdm_data['data'], 56, 136, 144), 2 )
+                        wgustdir = int( data_field_binary_string(aivdm_data['data'], 56, 145, 153), 2 )
+                        airtemp_string = data_field_binary_string(aivdm_data['data'], 56, 154, 164)
+                        airtemp = twos_comp( int(airtemp_string, 2), len(airtemp_string) ) / 10
+                        humidity = int( data_field_binary_string(aivdm_data['data'], 56, 165, 171), 2 )
+                        dewpoint_string = data_field_binary_string(aivdm_data['data'], 56, 172, 181)
+                        dewpoint = twos_comp( int(dewpoint_string, 2), len(dewpoint_string) ) / 10
+                        pressure = int( data_field_binary_string(aivdm_data['data'], 56, 182, 190), 2 )
+                        pressuretend = int( data_field_binary_string(aivdm_data['data'], 56, 191, 192), 2 )
+                        visgreater = int( data_field_binary_string(aivdm_data['data'], 56, 193, 193), 2 )
+                        visibility = int( data_field_binary_string(aivdm_data['data'], 56, 194, 200), 2 ) / 10
+                        waterlevel_string = data_field_binary_string(aivdm_data['data'], 56, 201, 212)
+                        waterlevel = twos_comp( int(waterlevel_string, 2), len(waterlevel_string) ) / 100
+                        leveltrend = int( data_field_binary_string(aivdm_data['data'], 56, 213, 214), 2 )
+                        cspeed = int( data_field_binary_string(aivdm_data['data'], 56, 215, 222), 2 ) / 10
+                        cdir = int( data_field_binary_string(aivdm_data['data'], 56, 223, 231), 2 )
+                        cspeed2 = int( data_field_binary_string(aivdm_data['data'], 56, 232, 239), 2 ) / 10
+                        cdir2 = int( data_field_binary_string(aivdm_data['data'], 56, 240, 248), 2 )
+                        cdepth2 = int( data_field_binary_string(aivdm_data['data'], 56, 249, 253), 2 )
+                        cspeed3 = int( data_field_binary_string(aivdm_data['data'], 56, 254, 261), 2 ) / 10
+                        cdir3 = int( data_field_binary_string(aivdm_data['data'], 56, 262, 270), 2 )
+                        cdepth3 = int( data_field_binary_string(aivdm_data['data'], 56, 271, 275), 2 )
+                        waveheight = int( data_field_binary_string(aivdm_data['data'], 56, 276, 283), 2 ) / 10
+                        waveperiod = int( data_field_binary_string(aivdm_data['data'], 56, 284, 289), 2 )
+                        wavedir = int( data_field_binary_string(aivdm_data['data'], 56, 290, 298), 2 )
+                        swellheight = int( data_field_binary_string(aivdm_data['data'], 56, 299, 306), 2 ) / 10
+                        swellperiod = int( data_field_binary_string(aivdm_data['data'], 56, 307, 312), 2 )
+                        swelldir = int( data_field_binary_string(aivdm_data['data'], 56, 313, 321), 2 )
+                        seastate = int( data_field_binary_string(aivdm_data['data'], 56, 322, 325), 2 )
+                        watertemp_string = data_field_binary_string(aivdm_data['data'], 56, 326, 335)
+                        watertemp = twos_comp( int(watertemp_string, 2), len(watertemp_string) ) / 10
+                        preciptype = int( data_field_binary_string(aivdm_data['data'], 56, 336, 338), 2 )
+                        salinity = int( data_field_binary_string(aivdm_data['data'], 56, 339, 347), 2 ) / 10
+                        ice = int( data_field_binary_string(aivdm_data['data'], 56, 348, 349), 2 )
+                        rt.logging.debug("mmsi", mmsi, "lon", lon, "lat", lat, "accuracy", accuracy, "day", day, "hour", hour, "minute", minute, "wspeed", wspeed, "wgust", wgust, "wdir", wdir, "wgustdir", wgustdir, "airtemp", airtemp, "humidity", humidity, "dewpoint", dewpoint, "pressure", pressure, "pressuretend", pressuretend, "visgreater", visgreater, "visibility", visibility, "waterlevel", waterlevel, "leveltrend", leveltrend, "cspeed", cspeed, "cdir", cdir, "cspeed2", cspeed2, "cdir2", cdir2, "cdepth2", cdepth2, "cspeed3", cspeed3, "cdir3", cdir3, "cdepth3", cdepth3, "waveheight", waveheight, "waveperiod", waveperiod, "wavedir", wavedir, "swellheight", swellheight, "swellperiod", swellperiod, "swelldir", swelldir, "seastate", seastate, "watertemp", watertemp, "preciptype", preciptype, "salinity", salinity, "ice", ice)
+
+                    #mmsis.append(mmsi)
+                except (TypeError, KeyError) as e :
+                    pass #rt.logging.exception(e)
+
+        return mmsis, latitudes, longitudes
