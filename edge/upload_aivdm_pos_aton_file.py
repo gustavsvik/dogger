18397b6dc907ef386120b40631da9b657e38dd39
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-05-08 19:01:26 +0000

    Update upload_aivdm_pos_aton_file.py

--- edge/upload_aivdm_pos_aton_file.py
+++ edge/upload_aivdm_pos_aton_file.py
@@ -11,7 +11,8 @@ udp_upload_ais = gateway.link.SqlFileAtonReport(
     width_offset = [-300, -50, 170, 270],
     mmsi = [992659999, 992659998, 992659997, 992659996],
     aid_type = [19, 30, 30, 19],
-    name = ["MEASUREMENT BEACON 1", "TEST AREA NO PASSAGE", "TEST AREA NO PASSAGE", "MEASUREMENT BEACON 2"],
+    # TODO: Investigate why whitespace character in first position of name extension field (bits 272-360) throws exception (bitstring.CreationError "uint cannot be initialsed by a negative number." raised in aislib.py function __setattr__)
+    name = ["MEASUREMENT BEACON 1 KEEP DISTANCE", "TEST AREA NO PASSAGE CH 16 7SA2026", "TEST AREA NO PASSAGE CH 16 7SA2026", "MEASUREMENT BEACON 2 KEEP DISTANCE"],
     virtual_aid = [0, 1, 1, 0],
     file_path = '/srv/dogger/files/',
     config_filepath = '/srv/dogger/', 
