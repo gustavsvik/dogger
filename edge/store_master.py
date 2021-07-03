7c38220bc5d92752b276de2b9cd3a46b3bbc0f2c
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-07-03 19:30:19 +0000

    Update store_master.py

--- edge/store_master.py
+++ edge/store_master.py
@@ -1,7 +1,7 @@
-import gateway.store
+import gateway.queue
 
 
-numpy_sql = gateway.store.NumpyFile(
+numpy_sql = gateway.queue.NumpyFile(
     channels = {143,146,147,168,169},
     start_delay = 0, 
     file_path = '/home/heta/Z/data/files/',
