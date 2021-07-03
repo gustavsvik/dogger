9767b81dfa273a1c02f282fd7441209a09fd6aa6
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-07-03 19:31:48 +0000

    Update store_text_string.py

--- edge/store_text_string.py
+++ edge/store_text_string.py
@@ -1,7 +1,7 @@
-import gateway.store
+import gateway.queue
 
 
-mob_store_text_sql = gateway.store.TextStringFile(
+mob_store_text_sql = gateway.queue.TextStringFile(
     channels = {142,144,145,150,151,162,163,167,171},
     start_delay = 0, 
     file_path = '/home/heta/Z/data/files/', 
