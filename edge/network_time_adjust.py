ed95b982ef19e9cb56589ae0d7448c46e500bd91
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-07-03 20:18:43 +0000

    Update network_time_adjust.py

--- edge/network_time_adjust.py
+++ edge/network_time_adjust.py
@@ -6,6 +6,7 @@ network_time = gateway.maint.NetworkTime(
     ntp_url = 'ntp.se',
     ntp_port = 123,
     adjust_interval = 600, 
+    max_connect_attempts = 50, 
     config_filepath = '/home/heta/Z/app/python/dogger/', 
     config_filename = 'conf.ini')
 
