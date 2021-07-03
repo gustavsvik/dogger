95e1ea411032f6ff7b7cb9044703e2b04345db43
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-07-03 20:19:18 +0000

    Update store_text_string_cloud.py

--- edge/store_text_string_cloud.py
+++ edge/store_text_string_cloud.py
@@ -1,8 +1,8 @@
-import gateway.store
+import gateway.queue
 
 
-mob_store_text_sql = gateway.store.TextStringFile(
-    channels = {61010,61013,170,148}, 
+mob_store_text_sql = gateway.queue.TextJsonFile(
+    channels = {61010,61013,170,148,172,173,149}, 
     start_delay = 0, 
     file_path = '/srv/dogger/files/',
     files_to_keep = 0,
