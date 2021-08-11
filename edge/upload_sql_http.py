34f32d02f8a36e80897bfdebbdaf1cc7b23486cc
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-08-11 00:19:29 +0000

    Update upload_sql_http.py

--- edge/upload_sql_http.py
+++ edge/upload_sql_http.py
@@ -2,7 +2,7 @@ import gateway.link
 
 
 sie_upload_sql_http = gateway.link.SqlHttp(
-    channels = {168,169,170,148,172,173}, 
+    channels = {168,169,170,148,172,173,152}, 
     start_delay = 0, 
     transmit_rate = 0.5,
     max_age = 10,
