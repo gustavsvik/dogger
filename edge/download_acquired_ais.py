144495bcbaa50a227ef27a86af1c0a9f90daabfe
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-08-11 00:20:38 +0000

    Update download_acquired_ais.py

--- edge/download_acquired_ais.py
+++ edge/download_acquired_ais.py
@@ -2,7 +2,7 @@ import gateway.link
 
 
 http_download = gateway.link.HttpSql(
-    channels = {144,145}, 
+    channels = {144,145,152}, 
     start_delay = 0, 
     transmit_rate = 1, 
     client_api_url = '/client/', 
