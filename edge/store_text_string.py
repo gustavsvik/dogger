19f0eef97c8f5ccb7c4b1c54eba38f1f105a338a
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-08-11 00:13:08 +0000

    Update store_text_string.py

--- edge/store_text_string.py
+++ edge/store_text_string.py
@@ -2,7 +2,7 @@ import gateway.queue
 
 
 mob_store_text_sql = gateway.queue.TextStringFile(
-    channels = {142,144,145,150,151,162,163,167,171},
+    channels = {142,144,145,150,151,162,163,164,167,171},
     start_delay = 0, 
     file_path = '/home/heta/Z/data/files/', 
     files_to_keep = 0,
