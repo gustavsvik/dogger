764f67787a268bd01d3140c9ad1e8c8fa8df62fe
Daniel Nilsson <s.m.daniel.nilsson@gmail.com>
2021-08-11 00:21:14 +0000

    Update upload_ais_udp_local.py

--- edge/upload_ais_udp_local.py
+++ edge/upload_ais_udp_local.py
@@ -2,7 +2,7 @@ import gateway.link
 
 
 udp_upload_local = gateway.link.SqlUdpNmeaLines(
-    channels = {144,145,170,172,173}, 
+    channels = {144,145,170,172,173,152}, 
     start_delay = 0,
     transmit_rate = 1, 
     port = 4444,
