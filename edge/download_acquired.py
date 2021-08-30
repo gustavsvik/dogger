c02ebd403c45a54df339cfdc5b99dde2270b3e97
heta <heta@HETA121073>
2021-08-30 12:14:20 +0200

    Level the load of fetching from cloud server to operator database

--- edge/download_acquired.py
+++ edge/download_acquired.py
@@ -2,12 +2,12 @@ import gateway.link
 
 
 http_download = gateway.link.HttpSql(
-    channels = {61011,61012,143}, 
-    start_delay = 0, 
-    transmit_rate = 1, 
-    client_api_url = '/client/', 
-    max_connect_attempts = 50, 
-    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    channels = {146,147,168,169},
+    start_delay = 0,
+    transmit_rate = 1,
+    client_api_url = '/client/',
+    max_connect_attempts = 50,
+    config_filepath = '/home/heta/Z/app/python/dogger/',
     config_filename = 'conf_opener.ini')
 
 http_download.run()
--- edge/download_acquired_3.py
+++ edge/download_acquired_3.py
@@ -2,12 +2,12 @@ import gateway.link
 
 
 http_download = gateway.link.HttpSql(
-    channels = {97,98}, 
-    start_delay = 0, 
-    transmit_rate = 1, 
-    client_api_url = '/client/', 
-    max_connect_attempts = 50, 
-    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    channels = {61011,61012},
+    start_delay = 0,
+    transmit_rate = 1,
+    client_api_url = '/client/',
+    max_connect_attempts = 50,
+    config_filepath = '/home/heta/Z/app/python/dogger/',
     config_filename = 'conf_opener.ini')
 
 http_download.run()
