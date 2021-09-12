2ae7472d2c369170f53fdd43f870eddfb0fd77f1
dannil10 <daniel.nilsson@gustavsvik.eu>
2021-09-12 09:46:25 +0200

    Removed some trailing whitespace

--- edge/acquire_ais_serial.py
+++ edge/acquire_ais_serial.py
@@ -4,19 +4,19 @@ import gateway.daqc
 usb_serial = gateway.daqc.SerialNmeaFile(
     channels = { 'VDM':{144:'txt'} , 'VDO':{145:'txt', 146:'npy', 147:'npy'} , 'ALV':{150:'txt'} , 'ALR':{151:'txt'} },
     ctrl_channels = { 'GGA':{164:'txt'} },
-    start_delay = 10, 
+    start_delay = 10,
     sample_rate = 1,
     location = '1-5',
-    baudrate = 115200, 
-    timeout = 0.01, 
+    baudrate = 115200,
+    timeout = 0.01,
     parity = 'E',
     stopbits = 1,
     bytesize = 8,
-    file_path = '/home/heta/Z/data/files/', 
+    file_path = '/home/heta/Z/data/files/',
     ctrl_file_path = '/home/heta/Z/data/files/others/',
     archive_file_path = '/home/heta/Z/data/files/',
     delay_waiting_check = 0,
-    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    config_filepath = '/home/heta/Z/app/python/dogger/',
     config_filename = 'conf.ini')
-    
+
 usb_serial.run()
--- edge/acquire_bar_serial.py
+++ edge/acquire_bar_serial.py
@@ -2,19 +2,19 @@ import gateway.daqc
 
 
 usb_serial = gateway.daqc.SerialNmeaFile(
-    channels = { 'MMB':{142:'txt',143:'npy'} }, 
-    start_delay = 10, 
+    channels = { 'MMB':{142:'txt',143:'npy'} },
+    start_delay = 10,
     sample_rate = 1,
     location = '1-2',
-    baudrate = 4800, 
-    timeout = 0.01, 
+    baudrate = 4800,
+    timeout = 0.01,
     parity = 'E',
     stopbits = 1,
     bytesize = 7,
-    file_path = '/home/heta/Z/data/files/', 
+    file_path = '/home/heta/Z/data/files/',
     archive_file_path = '/home/heta/Z/data/files/',
     delay_waiting_check = 0.01,
-    config_filepath = '/home/heta/Z/app/python/dogger/', 
+    config_filepath = '/home/heta/Z/app/python/dogger/',
     config_filename = 'conf.ini')
     
 usb_serial.run()
