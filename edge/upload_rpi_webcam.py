817d3a72fbb4e55002d81d62d1c53af953e5d8fb
dannil10 <daniel.nilsson@gustavsvik.eu>
2021-11-22 20:30:46 +0100

    Move overview camera mockup image from camera node to edge gateway

--- edge/upload_rpi_webcam.py
+++ edge/upload_rpi_webcam.py
@@ -2,11 +2,12 @@ import gateway.daqc
 
 
 screenshot = gateway.daqc.TempFileUpload(
-    channels = {140},
+    channels = {180},
     sample_rate = 1.0,
     host_api_url = '/host/',
     client_api_url = '/client/',
-    config_filepath = '/home/pi/webcam_ethernet/dogger/',
+    file_extension = 'jpg',
+    config_filepath = '/home/heta/Z/app/python/dogger/',
     config_filename = 'conf.ini')
 
 screenshot.run()
