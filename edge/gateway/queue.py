a2f876772907b6c5aaae3f488d50cc98844fdca3
dannil10 <daniel.nilsson@gustavsvik.eu>
2022-03-18 22:30:57 +0100

    Experiment with MessagePack serialization format

--- edge/gateway/queue.py
+++ edge/gateway/queue.py
@@ -7,6 +7,9 @@ import os
 import math
 import base64
 
+try : import msgpack_numpy
+except ImportError: pass
+
 import gateway.runtime as rt
 import gateway.persist as ps
 
@@ -18,9 +21,8 @@ class FileToSQL(ps.LoadFile):
     def __init__(self):
 
         ps.LoadFile.__init__(self)
-
         self.sql = ps.SQL(gateway_database_connection = self.gateway_database_connection, config_filepath = self.config_filepath, config_filename = self.config_filename)
-        
+
         if self.files_to_keep is None : self.files_to_keep = 2
 
 
@@ -202,6 +204,55 @@ class NumpyFile(FileToSQL):
                 subsample_string = numpy.array2string(acquired_values[2:], separator=' ', max_line_width = numpy.inf, formatter = {'float': lambda x: format(x, '6.5E')})
                 acquired_bytes = subsample_string[1:-1]
             acquired_value = acquired_values[0]
+            acquired_microsecs = acquired_values[1]
+        except (OSError, ValueError, IndexError) as e:
+            rt.logging.exception(e)
+            try:
+                os.remove(current_file)
+            except (PermissionError, FileNotFoundError, OSError) as e:
+                rt.logging.exception(e)
+
+        return acquired_microsecs, acquired_value, acquired_text, acquired_bytes
+
+
+
+class NumpyFileMsgpack(FileToSQL):
+
+
+    def __init__(self, channels = None, start_delay = None, gateway_database_connection = None, file_path = None, file_extensions = ['npy'], files_to_keep = None, config_filepath = None, config_filename = None):
+
+        self.channels = channels
+        self.start_delay = start_delay
+        self.gateway_database_connection = gateway_database_connection
+        self.file_path = file_path
+        self.file_extensions = file_extensions
+        self.files_to_keep = files_to_keep
+
+        self.config_filepath = config_filepath
+        self.config_filename = config_filename
+
+        FileToSQL.__init__(self)
+
+
+    def load_file(self, current_file = None):
+
+        acquired_microsecs = 9999
+        acquired_value = -9999.0
+        acquired_text = ''
+        acquired_bytes = b''
+
+        try:
+            acquired_values = numpy.load(current_file)
+            if len(acquired_values) > 1:
+                # acquired_bytes = base64.b64encode( (acquired_values[2:]).astype('float32', casting = 'same_kind') )
+                # acquired_text = numpy.array2string(acquired_values[2:], separator=' ', max_line_width = numpy.inf, formatter = {'float': lambda x: format(x, '6.5E')})
+                subsample_string = numpy.array2string(acquired_values[2:], separator=' ', max_line_width = numpy.inf, formatter = {'float': lambda x: format(x, '6.5E')})
+                packed = msgpack_numpy.packb(acquired_values[2:], default = msgpack_numpy.encode)
+                rt.logging.debug("packed", packed, len(packed), "len(packed)")
+                packed_ascii = base64.b64encode(packed)
+                rt.logging.debug("packed_ascii", packed_ascii, len(packed_ascii), "len(packed_ascii)")
+                acquired_bytes = packed_ascii
+            #acquired_value = acquired_values[0]
             acquired_microsecs = acquired_values[1]
         except (OSError, ValueError, IndexError) as e:
             rt.logging.exception(e)
