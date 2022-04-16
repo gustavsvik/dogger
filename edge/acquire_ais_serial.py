8b293eaf4b972e5690b8b3d4459518201f370171
dannil10 <daniel.nilsson@gustavsvik.eu>
2022-04-16 22:01:32 +0200

    Move to new edge gateway, add Gavazzi WM14 MODBUS register acquisition

--- edge/acquire_ais_serial.py
+++ edge/acquire_ais_serial.py
@@ -6,17 +6,17 @@ usb_serial = gateway.daqc.SerialNmeaFile(
     ctrl_channels = { 'GGA':{164:'txt'} },
     start_delay = 10,
     sample_rate = 1,
-    host_port = '1-5',
+    host_port = '1-1',
     serial_baudrate = 115200,
     serial_parity = gateway.daqc.Serial.PARITY_EVEN,
     serial_stopbits = gateway.daqc.Serial.STOPBITS_ONE,
     serial_bytesize = gateway.daqc.Serial.SEVENBITS,
     serial_timeout = 0.01,
     delay_waiting_check = 0,
-    file_path = '/home/heta/Z/data/files/',
-    ctrl_file_path = '/home/heta/Z/data/files/others/',
-    archive_file_path = '/home/heta/Z/data/files/',
-    config_filepath = '/home/heta/Z/app/python/dogger/',
+    file_path = '/home/scc01/Z/data/files/',
+    ctrl_file_path = '/home/scc01/Z/data/files/others/',
+    archive_file_path = '/home/scc01/Z/data/files/',
+    config_filepath = '/home/scc01/Z/dogger/',
     config_filename = 'conf.ini')
 
 usb_serial.run()
--- edge/store_text_string.py
+++ edge/store_text_string.py
@@ -2,11 +2,11 @@ import gateway.queue
 
 
 mob_store_text_sql = gateway.queue.TextStringFile(
-    channels = {142,144,145,150,151,162,163,164,167,171},
+    channels = {142,144,145,150,151,155,162,163,164,167,171},
     start_delay = 0,
-    file_path = '/home/heta/Z/data/files/',
+    file_path = '/home/scc01/Z/data/files/',
     files_to_keep = 0,
-    config_filepath = '/home/heta/Z/app/python/dogger/',
+    config_filepath = '/home/scc01/Z/dogger/',
     config_filename = 'conf.ini')
 
 mob_store_text_sql.run()
