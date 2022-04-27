ecda16636be7471466a18302c0d3239c39f36e34
dannil10 <daniel.nilsson@gustavsvik.eu>
2022-04-27 07:16:31 +0200

    Add consumption of MODBUS register data JSON files

--- edge/store_text_string_cloud.py
+++ edge/store_text_string_cloud.py
@@ -2,7 +2,7 @@ import gateway.queue
 
 
 mob_store_text_sql = gateway.queue.TextJsonFile(
-    channels = {61010,61013,170,148,172,173,149,152},
+    channels = {61010,61013,170,148,172,173,149,152,156},
     start_delay = 0,
     file_path = '/srv/dogger/files/',
     files_to_keep = 0,
