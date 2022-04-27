01bb7444d5a8b93f97f5a8fd066155a00af2bb2c
dannil10 <daniel.nilsson@gustavsvik.eu>
2022-04-27 07:14:07 +0200

    Separate consumption of text data files of MODBUS and AIS origin

--- edge/store_text_string.py
+++ edge/store_text_string.py
@@ -2,7 +2,7 @@ import gateway.queue
 
 
 mob_store_text_sql = gateway.queue.TextStringFile(
-    channels = {142,144,145,150,151,155,162,163,164,167,171},
+    channels = {142,150,151,155,157,162,163,164,167,171},
     start_delay = 0,
     file_path = '/home/scc01/Z/data/files/',
     files_to_keep = 0,
