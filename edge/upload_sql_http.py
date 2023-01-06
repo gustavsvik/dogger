82c675fb3ba912a274c252e37c3a97fd962e65c6
dannil10 <daniel.nilsson@gustavsvik.eu>
2023-01-06 23:52:41 +0100

    Additional HTTP relay channels, default conf file

--- edge/upload_sql_http.py
+++ edge/upload_sql_http.py
@@ -2,12 +2,10 @@ import gateway.link
 
 
 sie_upload_sql_http = gateway.link.SqlHttp(
-    channels = {168,169,170,148,152,156,172,173,61011,61012},
+    channels = {168,169,170,148,149,158,152,154,156,172,173,61011,61012},
     start_delay = 0,
     transmit_rate = 0.5,
     max_age = 10,
-    max_connect_attempts = 50,
-    config_filepath = '/srv/dogger/',
-    config_filename = 'conf.ini')
+    max_connect_attempts = 50)
 
 sie_upload_sql_http.run()
