6e0847eac2196f4b7e0aaff40649397d4afb8041
dannil10 <daniel.nilsson@gustavsvik.eu>
2023-01-06 23:56:27 +0100

    Use default config file

--- edge/upload_udp_bytes.py
+++ edge/upload_udp_bytes.py
@@ -3,13 +3,11 @@ import gateway.link
 
 mob_upload_sql_udp = gateway.link.SqlUdpRawBytes(
     #channels = {144},
-    channels = {144,145,150,151,155},
+    channels = {144,145,158,150,151,155},
     start_delay = 0,
     #transmit_rate = 1,
     transmit_rate = 0.5,
     port = 61011,
-    max_age = 10,
-    config_filepath = '/home/scc01/Z/dogger/',
-    config_filename = 'conf.ini')
+    max_age = 10)
 
 mob_upload_sql_udp.run()
