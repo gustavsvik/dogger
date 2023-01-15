ac8fff47263d85cb21e226c1e5e562a40c1429da
dannil10 <daniel.nilsson@gustavsvik.eu>
2023-01-15 13:38:59 +0100

    Include crop arguments set at mximum values

--- edge/acquire_rpi_webcam.py
+++ edge/acquire_rpi_webcam.py
@@ -3,12 +3,11 @@ import gateway.daqc
 
 usb_cam = gateway.daqc.USBCam(
     channels = {320},
-    start_delay = 0,
     sample_rate = 1.0,
     video_unit = '/dev/video0',
-    video_res = [1440, 1080],
-    video_crop_origin = [70, 140],
-    video_crop = [1350, 860],
+    video_res = [1080, 720],
+    video_crop_origin = [0, 0],
+    video_crop = [1080, 720],
     video_quality = 80,
     video_capture_method = 'PICAMERA2',
     video_rate = 5)
