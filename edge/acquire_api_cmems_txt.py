3b969b14d3f4fa31de2d234dfe6c3a0dda3bbb09
dannil10 <daniel.nilsson@gustavsvik.eu>
2023-06-08 11:58:38 +0200

    CMEMS updated API for Baltic wave height forecast

--- edge/acquire_api_cmems_txt.py
+++ edge/acquire_api_cmems_txt.py
@@ -6,7 +6,7 @@ cmems_acquire_task = gateway.daqc.NativeCmemsNumpyFile(
     start_delay = 0,
     sample_rate = 1/(60*20),
     service_id = 'BALTICSEA_ANALYSISFORECAST_WAV_003_010-TDS',
-    product_id = 'dataset-bal-analysis-forecast-wav-hourly',
+    product_id = 'cmems_mod_bal_wav_anfc_PT1h-i',
     out_file_name = 'BALTICSEA_ANALYSISFORECAST_WAV_003_010-TDS.nc',
     config_filename = 'conf_cmems_api.ini')
 
