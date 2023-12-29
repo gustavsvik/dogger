ecadac3b7c4fc1b52933ae6f72c93421e1e39a6e
dannil10 <daniel.nilsson@gustavsvik.eu>
2023-12-29 14:56:27 +0100

    Namespace reorganize, set compile time log level

--- edge/node/esp8266/modbus_wifi_udp/include/LogSerial.h
+++ edge/node/esp8266/modbus_wifi_udp/include/LogSerial.h
@@ -1,27 +1,39 @@
 #ifndef _LOGSERIAL_h
 #define _LOGSERIAL_h
 
-namespace Bus::HardSerial
+#define LOGLEVEL 1
+
+namespace Bus::SerialBus::Hard
 {
   class Log
   {
     public:
-      Log(int a_baud_rate, int a_log_level = 1);
-      void write(int a_logged_int, int a_log_level = 1);
-      void write(char a_logged_char, int a_log_level = 1);
-      void write(const char* a_logged_string, int a_log_level = 1);
-      void write(String a_logged_string, int a_log_level = 1);
-      void write_hex(int a_logged_int, int a_log_level = 1);
-      void linefeed(int a_log_level = 1);
-      void writeln(int a_logged_int, int a_log_level = 1);
-      void writeln(char a_logged_char, int a_log_level = 1);
-      void writeln(const char* a_logged_string, int a_log_level = 1);
-      void writeln(String a_logged_string, int a_log_level = 1);
-      void writeln_hex(int a_logged_int, int a_log_level = 1);
-
+      #ifdef LOGLEVEL
+      Log(int a_baud_rate = 9600, int a_log_level = LOGLEVEL);
+      void write(int a_logged_int, int a_log_level = LOGLEVEL);
+      void write(char a_logged_char, int a_log_level = LOGLEVEL);
+      void write(const char* a_logged_string, int a_log_level = LOGLEVEL);
+      void write(String a_logged_string, int a_log_level = LOGLEVEL);
+      void write_hex(int a_logged_int, int a_log_level = LOGLEVEL);
+      void linefeed(int a_log_level = LOGLEVEL);
+      void writeln(int a_logged_int, int a_log_level = LOGLEVEL);
+      void writeln(char a_logged_char, int a_log_level = LOGLEVEL);
+      void writeln(const char* a_logged_string, int a_log_level = LOGLEVEL);
+      void writeln(String a_logged_string, int a_log_level = LOGLEVEL);
+      void writeln_hex(int a_logged_int, int a_log_level = LOGLEVEL);
+      #else
+      Log(...);
+      void write(...);
+      void write_hex(...);
+      void linefeed(...);
+      void writeln(...);
+      void writeln_hex(...);
+      #endif
     private:
+      #ifdef LOGLEVEL
       int m_baud_rate;
-      int m_log_level;
+      int m_log_level = LOGLEVEL;
+      #endif
   };
 }
 
--- edge/node/esp8266/modbus_wifi_udp/src/LogSerial.cpp
+++ edge/node/esp8266/modbus_wifi_udp/src/LogSerial.cpp
@@ -3,62 +3,75 @@
 #include <LogSerial.h>
 
 
-Bus::HardSerial::Log::Log(int a_baud_rate, int a_log_level) : m_baud_rate(a_baud_rate), m_log_level(a_log_level)
+#ifdef LOGLEVEL
+
+Bus::SerialBus::Hard::Log::Log(int a_baud_rate, int a_log_level) : m_baud_rate(a_baud_rate), m_log_level(a_log_level)
 {
   Serial.begin(this->m_baud_rate);
 }
 
-void Bus::HardSerial::Log::write(int a_logged_int, int a_log_level) 
+void Bus::SerialBus::Hard::Log::write(int a_logged_int, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.print(a_logged_int);
 }
 
-void Bus::HardSerial::Log::write(const char* a_logged_string, int a_log_level) 
+void Bus::SerialBus::Hard::Log::write(const char* a_logged_string, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.print(a_logged_string);
 }
 
-void Bus::HardSerial::Log::write(char a_logged_char, int a_log_level) 
+void Bus::SerialBus::Hard::Log::write(char a_logged_char, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.print(a_logged_char);
 }
 
-void Bus::HardSerial::Log::write(String a_logged_string, int a_log_level) 
+void Bus::SerialBus::Hard::Log::write(String a_logged_string, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.print(a_logged_string);
 }
 
-void Bus::HardSerial::Log::write_hex(int a_logged_int, int a_log_level) 
+void Bus::SerialBus::Hard::Log::write_hex(int a_logged_int, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.print(a_logged_int, HEX);
 }
 
-void Bus::HardSerial::Log::linefeed(int a_log_level) 
+void Bus::SerialBus::Hard::Log::linefeed(int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.println();
 }
 
-void Bus::HardSerial::Log::writeln(int a_logged_int, int a_log_level) 
+void Bus::SerialBus::Hard::Log::writeln(int a_logged_int, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.println(a_logged_int);
 }
 
-void Bus::HardSerial::Log::writeln(const char* a_logged_string, int a_log_level) 
+void Bus::SerialBus::Hard::Log::writeln(const char* a_logged_string, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.println(a_logged_string);
 }
 
-void Bus::HardSerial::Log::writeln(char a_logged_char, int a_log_level) 
+void Bus::SerialBus::Hard::Log::writeln(char a_logged_char, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.println(a_logged_char);
 }
 
-void Bus::HardSerial::Log::writeln(String a_logged_string, int a_log_level) 
+void Bus::SerialBus::Hard::Log::writeln(String a_logged_string, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.println(a_logged_string);
 }
 
-void Bus::HardSerial::Log::writeln_hex(int a_logged_int, int a_log_level) 
+void Bus::SerialBus::Hard::Log::writeln_hex(int a_logged_int, int a_log_level) 
 { 
   if (a_log_level >= this->m_log_level) Serial.println(a_logged_int, HEX);
 }
+
+#else
+
+Bus::SerialBus::Hard::Log::Log(...) {}
+void Bus::SerialBus::Hard::Log::write(...) {}
+void Bus::SerialBus::Hard::Log::write_hex(...) {}
+void Bus::SerialBus::Hard::Log::linefeed(...) {}
+void Bus::SerialBus::Hard::Log::writeln(...) {}
+void Bus::SerialBus::Hard::Log::writeln_hex(...) {}
+
+#endif
